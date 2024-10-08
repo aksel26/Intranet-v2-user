4cbb16fdc97a67e47a107ea158b84129588b7463
aksel26 <kevinxkim2023@gmail.com>
2024-10-08 18:57:33 +0900

    sample zustand

---
+++ src/app/store/zustandStord.ts
@@ -0,0 +1,14 @@
+// app/store/zustandStore.ts
+import { create } from "zustand";
+
+interface BearState {
+  bears: number;
+  increase: () => void;
+  decrease: () => void;
+}
+
+export const useBearStore = create<BearState>((set) => ({
+  bears: 0,
+  increase: () => set((state) => ({ bears: state.bears + 1 })),
+  decrease: () => set((state) => ({ bears: state.bears - 1 })),
+}));
