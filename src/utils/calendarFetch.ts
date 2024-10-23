6d1420b4d8d141671cf0d747744819e93c18282f
aksel26 <kevinxkim2023@gmail.com>
2024-10-23 18:48:21 +0900

    calendar Event Util

---
+++ src/utils/calendarFetch.ts
@@ -0,0 +1,9 @@
+export const titleRender = (input: string) => {
+  if (input === "근무") {
+    return "🍙";
+  } else if (input === "반차") {
+    return "🕑";
+  } else if (input === "연차") {
+    return "🏝️";
+  }
+};
