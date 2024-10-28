6c09f0eab8976326d9455830026d520a85a0d6e7
aksel26 <kevinxkim2023@gmail.com>
2024-10-28 15:26:55 +0900

    금액 관련 공용화

--- src/components/content/meal/MealBalance.tsx
+++ src/components/content/meal/MealBalance.tsx
@@ -1,13 +1,22 @@
 "use client";
 
-import { ChartSummary } from "@/template/ChartSummary";
-import { Group, Image, NumberFormatter, Paper, Stack, Text } from "@mantine/core";
 import { mealStore } from "@/lib/store/mealStore";
-import NextImage from "next/image";
-import myImage from "../../../../public/images/Onigiri.png";
+import { ChartSummary } from "@/template/ChartSummary";
+import { Group, NumberFormatter, Paper, Stack, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 export const MealBalance = () => {
-  const { mealBalance, userName } = mealStore((state) => state.mealInfo.mealStats);
+  const [statsInfo, setStatsInfo] = useState<any>({
+    balance: 0,
+    budget: 0,
+    expenses: 0,
+  });
+  const stats = mealStore((state) => state.mealInfo.mealStats);
+
+  useEffect(() => {
+    setStatsInfo((prev: any) => ({ ...prev, balance: stats.mealBalance, budget: stats.mealBudget, expenses: stats.mealExpense }));
+  }, [stats]);
+
   return (
     <Paper radius="lg" p="sm" py={"md"}>
       <Stack gap={0}>
@@ -22,20 +31,20 @@ export const MealBalance = () => {
             <Text fw={700}>
               안녕하세요,
               <Text component="span" fw={700} c={"blue.9"} mr={5}>
-                {userName}
+                {stats.userName}
               </Text>
               님,
             </Text>
             <Text>
               이번달은
               <Text mx={5} component="span" c={"blue.9"} fw={700} size="xl">
-                <NumberFormatter thousandSeparator value={mealBalance || 0} suffix=" 원" />
+                <NumberFormatter thousandSeparator value={stats.mealBalance || 0} suffix=" 원" />
               </Text>
               남으셨네요!
             </Text>
           </Stack>
         </Group>
-        <ChartSummary />
+        <ChartSummary stats={statsInfo} />
       </Stack>
     </Paper>
   );
