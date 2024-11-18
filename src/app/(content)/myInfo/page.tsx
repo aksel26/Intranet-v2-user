756b9dc29ef4e456e8aac453e6c5567e1263afa4
aksel26 <kevinxkim2023@gmail.com>
2024-11-18 18:40:39 +0900

    내 정보 수정

--- src/app/(content)/myInfo/page.tsx
+++ src/app/(content)/myInfo/page.tsx
@@ -1,19 +1,87 @@
 "use client";
-import { ActionIcon, Button, Container, Flex, Group, NavLink, PasswordInput, Stack, Text, TextInput } from "@mantine/core";
+import notification from "@/components/GNB/Notification";
+import { useChangeMyInfo, useChangePassword } from "@/hooks/useSubmitForm";
+import { formatPhoneNumber } from "@/utils/phoneNumber";
+import { Button, Container, Flex, Group, NavLink, PasswordInput, Stack, Text, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useRouter } from "next/navigation";
-import Back from "/public/icons/arrow-left.svg";
+import { useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+interface ErrorResponse {
+  message: string;
+  code?: string;
+  details?: string;
+}
 
 const MyInfo = () => {
-  const form = useForm({
-    mode: "uncontrolled",
+  const queryClient = useQueryClient();
+  const { mutate: changePassword } = useChangePassword();
+  const { mutate: changeMyInfo } = useChangeMyInfo();
+
+  const [prePasswordErr, setPrePasswordErr] = useState(false);
+
+  const userInfoform = useForm({
     initialValues: {
-      userCell: "",
       userAddress: "",
+      userCell: "",
+    },
+    validate: {
+      userCell: (value) => {
+        if (!value) return "휴대폰 번호를 입력해주세요";
+        if (value.replace(/-/g, "").length !== 11) {
+          return "올바른 휴대폰 번호를 입력해주세요";
+        }
+        return null;
+      },
     },
   });
-  const router = useRouter();
-  const goBack = () => router.back();
+
+  const passwordForm = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      prePassword: "",
+      newPassword: "",
+      confirmPassword: "",
+    },
+    validate: {
+      confirmPassword: (value, values) => (value !== values.newPassword ? "비밀번호가 일치하지 않습니다." : null),
+    },
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const formatted = formatPhoneNumber(event.target.value);
+    userInfoform.setFieldValue("userCell", formatted);
+  };
+
+  const submitChangePassword = (values: any) => {
+    changePassword(values, {
+      onSuccess: () => {
+        notification({ title: "비밀번호 변경", color: "green", message: "비밀번호가 변경되었습니다." });
+        setPrePasswordErr(false);
+        passwordForm.reset();
+      },
+      onError: (error) => {
+        if (error.message) {
+          setPrePasswordErr(true);
+        }
+      },
+    });
+  };
+
+  const submitChangeMyInfo = (values: any) => {
+    changeMyInfo(values, {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries({ queryKey: ["me"] });
+        notification({ title: "내 정보 변경", color: "green", message: "내 정보가 변경되었습니다." });
+
+        userInfoform.reset();
+      },
+      onError: (error) => {
+        if (error.message) {
+          notification({ title: "내 정보 변경", color: "red", message: "내 정보 변경을 실패하였습니다." });
+        }
+      },
+    });
+  };
   return (
     <Container size={"lg"} p={0} style={{ scrollPaddingBottom: "52px", overflowY: "auto", scrollSnapType: "y mandatory" }}>
       <Flex direction={"column"} pt={"lg"} p={"sm"}>
@@ -22,27 +90,71 @@ const MyInfo = () => {
         </Text>
 
         <NavLink label="기본정보 변경" childrenOffset={28} mt={"lg"} py={"md"}>
-          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <form onSubmit={userInfoform.onSubmit(submitChangeMyInfo)}>
             <Stack py={"md"}>
-              <TextInput withAsterisk label="연락처" value={"010-2222-1111"} key={form.key("userCell")} {...form.getInputProps("userCell")} />
-              <TextInput withAsterisk label="주소" value={"서울시 용산구 서빙고로 18"} key={form.key("userAddress")} {...form.getInputProps("userAddress")} />
+              <TextInput
+                value={userInfoform.values.userCell}
+                onChange={handleChange}
+                error={userInfoform.errors.userCell}
+                label="연락처"
+                withAsterisk
+                placeholder="010-0000-0000"
+                maxLength={13}
+                styles={{
+                  input: {
+                    letterSpacing: "1px",
+                  },
+                }}
+              />
+              <TextInput
+                placeholder="주소를 입력해 주세요."
+                withAsterisk
+                label="주소"
+                key={userInfoform.key("userAddress")}
+                {...userInfoform.getInputProps("userAddress")}
+              />
 
               <Group justify="end">
-                <Button size="sm">변경하기</Button>
+                <Button type="submit" size="sm">
+                  변경하기
+                </Button>
               </Group>
             </Stack>
           </form>
         </NavLink>
 
         <NavLink label="비밀번호 변경" childrenOffset={28} py={"md"}>
-          <Stack py={"md"}>
-            <PasswordInput label="기존 비밀번호" withAsterisk placeholder="기존 비밀번호" />
-            <PasswordInput label="새 비밀번호" withAsterisk placeholder="새 비밀번호" />
-            <PasswordInput label="새 비밀번호 확인" withAsterisk placeholder="새 비밀번호 확인" />
-            <Group justify="end">
-              <Button size="sm">변경하기</Button>
-            </Group>
-          </Stack>
+          <form onSubmit={passwordForm.onSubmit(submitChangePassword)}>
+            <Stack py={"md"}>
+              <PasswordInput
+                label="기존 비밀번호"
+                withAsterisk
+                placeholder="기존 비밀번호"
+                key={passwordForm.key("prePassword")}
+                {...passwordForm.getInputProps("prePassword")}
+                error={prePasswordErr && "기존 비밀번호가 올바르지 않습니다."}
+              />
+              <PasswordInput
+                label="새 비밀번호"
+                withAsterisk
+                placeholder="새 비밀번호"
+                key={passwordForm.key("newPassword")}
+                {...passwordForm.getInputProps("newPassword")}
+              />
+              <PasswordInput
+                label="새 비밀번호 확인"
+                withAsterisk
+                placeholder="새 비밀번호 확인"
+                key={passwordForm.key("confirmPassword")}
+                {...passwordForm.getInputProps("confirmPassword")}
+              />
+              <Group justify="end">
+                <Button type="submit" size="sm">
+                  변경하기
+                </Button>
+              </Group>
+            </Stack>
+          </form>
         </NavLink>
       </Flex>
     </Container>
