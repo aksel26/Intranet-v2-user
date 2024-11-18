586d05aac3a87280ab062419d251388bf37d1014
aksel26 <kevinxkim2023@gmail.com>
2024-11-18 18:41:58 +0900

    test

--- src/app/(content)/welfare/mypage/myInfo/page.tsx
+++ src/app/(content)/welfare/mypage/myInfo/page.tsx
@@ -5,15 +5,30 @@ import { useRouter } from "next/navigation";
 import Back from "/public/icons/arrow-left.svg";
 
 const MyInfo = () => {
-  const form = useForm({
-    mode: "uncontrolled",
-    initialValues: {
-      userCell: "",
-      userAddress: "",
-    },
-  });
+  // const form = useForm({
+  //   mode: "uncontrolled",
+  //   initialValues: {
+  //     userCell: "",
+  //     userAddress: "",
+  //   },
+  // });
+
+  // const passwordForm = useForm({
+  //   mode: "uncontrolled",
+  //   initialValues: {
+  //     newPassword: "",
+  //     confirmPassword: "",
+  //   },
+  //   validate: {
+  //     confirmPassword: (value, values) => (value !== values.newPassword ? "비밀번호가 일치하지 않습니다." : null),
+  //   },
+  // });
   const router = useRouter();
   const goBack = () => router.back();
+
+  const changePassword = (values: any) => {
+    console.log(values);
+  };
   return (
     <Flex direction={"column"} pt={"lg"} p={"sm"}>
       <Group gap={"xs"}>
@@ -24,40 +39,7 @@ const MyInfo = () => {
           내 정보 수정
         </Text>
       </Group>
-
-      <NavLink label="기본정보 변경" childrenOffset={28} mt={"lg"} py={"md"}>
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
-          <Stack py={"md"}>
-            <TextInput withAsterisk label="연락처" value={"010-2222-1111"} key={form.key("userCell")} {...form.getInputProps("userCell")} />
-            <TextInput withAsterisk label="주소" value={"서울시 용산구 서빙고로 18"} key={form.key("userAddress")} {...form.getInputProps("userAddress")} />
-
-            <Group justify="end">
-              <Button size="sm">변경하기</Button>
-            </Group>
-          </Stack>
-        </form>
-      </NavLink>
-
-      <NavLink label="비밀번호 변경" childrenOffset={28} py={"md"}>
-        <Stack py={"md"}>
-          <PasswordInput label="기존 비밀번호" withAsterisk placeholder="기존 비밀번호" />
-          <PasswordInput label="새 비밀번호" withAsterisk placeholder="새 비밀번호" />
-          <PasswordInput label="새 비밀번호 확인" withAsterisk placeholder="새 비밀번호 확인" />
-          <Group justify="end">
-            <Button size="sm">변경하기</Button>
-          </Group>
-        </Stack>
-      </NavLink>
-      {/* 
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <TextInput withAsterisk label="Email" placeholder="your@email.com" key={form.key("email")} {...form.getInputProps("email")} />
-
-        <Checkbox mt="md" label="I agree to sell my privacy" key={form.key("termsOfService")} {...form.getInputProps("termsOfService", { type: "checkbox" })} />
-
-        <Group justify="flex-end" mt="md">
-          <Button type="submit">Submit</Button>
-        </Group>
-      </form> */}
+      없앨까
     </Flex>
   );
 };
