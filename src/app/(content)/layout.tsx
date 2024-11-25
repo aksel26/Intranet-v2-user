01c7127680eaf96c64a100d5ba68b314f9deb5e7
aksel26 <kevinxkim2023@gmail.com>
2024-11-25 18:40:56 +0900

    nav 구성 변경

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Footer } from "@/components/GNB/Footer";
-import { AppShell, AppShellFooter, Badge, Box, Burger, Button, Container, Flex, Group, Image, NavLink, Skeleton, Stack, Text } from "@mantine/core";
+import { AppShell, AppShellFooter, Badge, Box, Burger, Button, Center, Container, Flex, Group, Image, NavLink, Skeleton, Stack, Text } from "@mantine/core";
 // import Image from "next/image";
 import useLogout from "@/hooks/useLogout";
 import { useDisclosure } from "@mantine/hooks";
@@ -48,31 +48,30 @@ export default function ContentLayout({ children }: { children: React.ReactNode 
     });
   };
 
+  const clickLogo = () => router.push("/main");
+
   return (
     <AppShell
       header={{
         height: 50,
       }}
       navbar={{ width: 350, breakpoint: "sm", collapsed: { mobile: !mobileOpened, desktop: !desktopOpened } }}
-      footer={{
-        height: 70,
-      }}
       layout="alt"
       // pr={!desktopOpened ? 0 : 300}
     >
       <AppShell.Header withBorder={false}>
         {/* <Header opened={opened} toggle={toggle} /> */}
-        <Group h={"100%"} py={"lg"} px={"xl"}>
+        <Group h={"100%"} px={"sm"}>
           <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
           <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
         </Group>
       </AppShell.Header>
-      <AppShell.Main>{children}</AppShell.Main>
+      <AppShell.Main styles={{ main: { background: "#f2f2f2" } }}>{children}</AppShell.Main>
       <AppShell.Navbar p="md" withBorder={false}>
         <Group justify="space-between" mb={"lg"}>
           <Group>
             <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-            <Image component={NextImage} src={myImage} alt="My image" fit="contain" h={20} w={80} style={{ cursor: "pointer" }} />
+            <Image onClick={clickLogo} component={NextImage} src={myImage} alt="My image" fit="contain" h={20} w={80} style={{ cursor: "pointer" }} />
           </Group>
 
           <Button size="xs" variant="light" onClick={handleLogout}>
@@ -83,11 +82,11 @@ export default function ContentLayout({ children }: { children: React.ReactNode 
           <Flex w={"100%"} bg={"blue.0"} align={"center"} mih={100} columnGap={"xl"} p={"md"} style={{ position: "relative", borderRadius: 7 }}>
             <Flex direction={"column"} rowGap={"md"} w={"100%"}>
               <Box>
-                <Text size={"xl"} fw={700} c={"blue.9"}>
+                <Text fz={"lg"} fw={700} c={"blue.9"}>
                   {user?.userName}
                 </Text>
-                <Text c={"blue.9"}>
-                  <Text c={"blue.9"} component="span" mr={5}>
+                <Text c={"blue.9"} fz={"sm"}>
+                  <Text fz={"sm"} c={"blue.9"} component="span" mr={5}>
                     {user?.teamName || user?.hqName}
                   </Text>
                   {user?.gradeName}
@@ -111,19 +110,17 @@ export default function ContentLayout({ children }: { children: React.ReactNode 
             </Flex>
           </Flex>
         </Skeleton>
-        <Group justify="space-between" my={"lg"} px={"sm"}>
-          <Group>
-            <Text size="sm">업무 경과시간</Text>
-            <Text size="sm" styles={{ root: { letterSpacing: 1.2 } }}>
-              12:22:00
-            </Text>
-          </Group>
-          <Badge size="lg" color="blue" radius="md">
-            정상출근
-          </Badge>
+
+        <Group wrap="nowrap" my="sm">
+          <Button fullWidth variant="filled">
+            출근하기
+          </Button>
+          <Button fullWidth variant="filled">
+            퇴근하기
+          </Button>
         </Group>
         <NavLink label="근태관리" childrenOffset={28}>
-          <NavLink label="근태 · 휴가" />
+          <NavLink component={Link} href={"/attendance"} label="근태 · 휴가" />
         </NavLink>
         <NavLink label="검사현황" />
         <NavLink component={Link} label="복지" href={"/welfare/meal"}>
@@ -135,11 +132,12 @@ export default function ContentLayout({ children }: { children: React.ReactNode 
 
         <NavLink component={Link} label="내 정보" href={"/myInfo"} />
       </AppShell.Navbar>
-      <AppShellFooter withBorder={false}>
+      {/* <AppShellFooter withBorder={false}>
         <Container size="xs" style={{ margin: "0 auto", borderTop: "1px solid #e9e9e9" }} h={"100%"}>
-          <Footer />
+
+          <Center>ACG</Center>
         </Container>
-      </AppShellFooter>
+      </AppShellFooter> */}
     </AppShell>
   );
 }
