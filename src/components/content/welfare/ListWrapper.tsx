db18b0402db75e75fe7c0ea8af81210e8571baf8
aksel26 <kevinxkim2023@gmail.com>
2024-11-26 17:58:41 +0900

    add icons

--- src/components/content/welfare/ListWrapper.tsx
+++ src/components/content/welfare/ListWrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const ListWrapper = ({ children }: { children: any }) => {
   return (
-    <Flex w={"100%"} direction={"column"} py={"md"}>
+    <Flex w={"100%"} direction={"column"} pb={"md"} gap={"sm"}>
       {children}
     </Flex>
   );
