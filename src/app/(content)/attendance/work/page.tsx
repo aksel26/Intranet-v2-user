693ca67b83150ec4de3f2e7e3cf12ed46dc4094f
aksel26 <kevinxkim2023@gmail.com>
2024-11-29 15:07:35 +0900

    근태관리 페이지

---
+++ src/app/(content)/attendance/work/page.tsx
@@ -0,0 +1,21 @@
+import { Breadcrumbs, Container, Text } from "@mantine/core";
+import React from "react";
+const items = [
+  { title: "근태/휴가 관리", href: "#" },
+  { title: "근태관리", href: "#" },
+].map((item, index) => (
+  <Text size="lg" fw={700} component="a" key={index}>
+    {/* <Anchor href={item.href} key={index}> */}
+    {item.title}
+    {/* </Anchor> */}
+  </Text>
+));
+function page() {
+  return (
+    <Container fluid p={"lg"} style={{ scrollPaddingBottom: "52px", overflowY: "auto", scrollSnapType: "y mandatory" }}>
+      <Breadcrumbs mb={"md"}>{items}</Breadcrumbs>
+    </Container>
+  );
+}
+
+export default page;
