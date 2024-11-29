b911f9dd1c21b6cad2e53db79b1a1e5567d49d37
aksel26 <kevinxkim2023@gmail.com>
2024-11-29 15:08:47 +0900

    공지사항 페이지

--- src/app/(content)/notice/[id]/page.tsx
+++ src/app/(content)/notice/[id]/page.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+"use client";
 
+import { Breadcrumbs, Container, Group, Input, Paper, Stack, Text, Textarea } from "@mantine/core";
+import React from "react";
+const items = [
+  { title: "공지사항", href: "#" },
+  { title: "공지사항 상세내용", href: "#" },
+].map((item, index) => (
+  <Text size="lg" fw={700} component="a" key={index}>
+    {item.title}
+  </Text>
+));
 function page() {
-  return <div>공지사항 상세 내용</div>;
+  return (
+    <Container fluid p={"lg"} style={{ scrollPaddingBottom: "52px", overflowY: "auto", scrollSnapType: "y mandatory" }}>
+      <Breadcrumbs mb={"md"}>{items}</Breadcrumbs>
+
+      <Paper bg={"white"} px="md" py="lg" radius={"lg"} h={"100%"}>
+        <Stack>
+          <Group justify="space-between">
+            <Input.Wrapper label="작성자">
+              <Input value={"P&C팀 안지훈 위원"} readOnly variant="unstyled" />
+            </Input.Wrapper>
+            <Input.Wrapper label="작성일">
+              <Input value={"2024-12-12"} readOnly variant="unstyled" />
+            </Input.Wrapper>
+          </Group>
+          <Input.Wrapper label="제목">
+            <Input
+              value={
+                "Non quis irure laboris ex pariatur consequat esse fugiat exercitation ad non. Elit do incididunt aliquip adipisicing non. Sint amet adipisicing excepteur magna irure fugiat deserunt in dolor. Laborum consequat nisi culpa. Consequat sunt eu ipsum."
+              }
+              readOnly
+              variant="unstyled"
+            />
+          </Input.Wrapper>
+
+          <Textarea
+            label="공지 내용"
+            readOnly
+            autosize
+            value={
+              "Adipisicing sint aliquip enim do sunt adipisicing in occaecat eu laborum minimAdipisicing sint aliquip enim do sunt adipisicing in occaecat eu laborum minimAdipisicing sint aliquip enim do sunt adipisicing in occaecat eu laborum minimAdipisicing sint aliquip enim do sunt adipisicing in occaecat eu laborum minim."
+            }
+            variant="unstyled"
+            styles={{ root: { height: "auto" } }}
+          />
+        </Stack>
+      </Paper>
+    </Container>
+  );
 }
 
 export default page;
--- src/app/(content)/notice/page.tsx
+++ src/app/(content)/notice/page.tsx
@@ -1,40 +1,90 @@
 "use client";
-import { Container, Group, Pagination, Table, Text } from "@mantine/core";
+import { Box, Breadcrumbs, Container, Divider, Group, List, ListItem, Pagination, Paper, Table, Text } from "@mantine/core";
+import { usePathname, useRouter } from "next/navigation";
 
 const elements = Array.from({ length: 15 }, (_, index) => {
   return { position: index + 1, title: "제목1", writer: "작성지2", createdAt: "2024-10-11" };
 });
 
 const Notice = () => {
-  const rows = elements.map((element: any, index: number) => (
-    <Table.Tr key={index + 1}>
-      <Table.Td>{index + 1}</Table.Td>
-      <Table.Td>{element.title}</Table.Td>
-      <Table.Td>{element.writer}</Table.Td>
-      <Table.Td>{element.createdAt}</Table.Td>
-    </Table.Tr>
+  const items = [{ title: "공지사항", href: "#" }].map((item, index) => (
+    <Text size="lg" fw={700} component="a" key={index}>
+      {item.title}
+    </Text>
   ));
-  return (
-    <Container size={"lg"} py={"md"} style={{ scrollPaddingBottom: "52px", overflowY: "auto", scrollSnapType: "y mandatory" }}>
-      {/* <Flex direction={"column"} pt={"lg"} p={"sm"}> */}
-      <Text size="xl" fw={700}>
-        공지사항
-      </Text>
 
-      <Table my={"md"} highlightOnHover withRowBorders>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>No.</Table.Th>
-            <Table.Th>제목</Table.Th>
-            <Table.Th>작성자</Table.Th>
-            <Table.Th>작성일</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
-      <Group justify="center">
-        <Pagination total={10} />
-      </Group>
+  const router = useRouter();
+  const pathName = usePathname();
+  const goDetail = (id: number) => {
+    router.push(`${pathName}/${id}`);
+  };
+  return (
+    <Container fluid p={"lg"} style={{ scrollPaddingBottom: "52px", overflowY: "auto", scrollSnapType: "y mandatory" }}>
+      {/* <Flex direction={"column"} pt={"lg"} p={"sm"}> */}
+      <Breadcrumbs mb={"md"}>{items}</Breadcrumbs>
+
+      <Paper bg={"white"} px="md" py="lg" radius={"lg"}>
+        <Text c={"dimmed"} fz={"sm"}>
+          공지사항 등록은 P&C팀에게 문의해 주시기 바랍니다.
+        </Text>
+        <Divider my={"md"} />
+        <List w={"100%"} spacing="xs" size="sm" center styles={{ itemLabel: { width: "100%" }, itemWrapper: { width: "100%" } }}>
+          <ListItem w={"100%"} onClick={() => goDetail(2)}>
+            <Group justify="space-between">
+              <Box>
+                <Group>
+                  <Text fz={"xs"}>1</Text>
+                  <Text fz={"xs"}>ACG Monthly Meeting</Text>
+                </Group>
+              </Box>
+              <Group>
+                <Text c={"dimmed"} fz={"xs"}>
+                  안지훈
+                </Text>
+                <Text c={"dimmed"} fz={"xs"}>
+                  2024-12-12
+                </Text>
+              </Group>
+            </Group>
+          </ListItem>
+          <ListItem w={"100%"}>
+            <Group justify="space-between">
+              <Box>
+                <Group>
+                  <Text fz={"xs"}>2</Text>
+                  <Text fz={"xs"}>ACG Monthly Meeting</Text>
+                </Group>
+              </Box>
+              <Group>
+                <Text c={"dimmed"} fz={"xs"}>
+                  안지훈
+                </Text>
+                <Text c={"dimmed"} fz={"xs"}>
+                  2024-12-12
+                </Text>
+              </Group>
+            </Group>
+          </ListItem>
+          <ListItem w={"100%"}>
+            <Group justify="space-between">
+              <Box>
+                <Group>
+                  <Text fz={"xs"}>3</Text>
+                  <Text fz={"xs"}>ACG Monthly Meeting</Text>
+                </Group>
+              </Box>
+              <Group>
+                <Text c={"dimmed"} fz={"xs"}>
+                  안지훈
+                </Text>
+                <Text c={"dimmed"} fz={"xs"}>
+                  2024-12-12
+                </Text>
+              </Group>
+            </Group>
+          </ListItem>
+        </List>
+      </Paper>
 
       {/* </Flex> */}
     </Container>
