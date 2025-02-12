0e02c404b1fc031af043c948a5bfcc5d6c51578f
aksel26 <kevinxkim2023@gmail.com>
2025-02-12 15:59:51 +0900

    출퇴근 버튼 disabled 조건

--- src/components/Navbar/AttendanceButtonWrapper.tsx
+++ src/components/Navbar/AttendanceButtonWrapper.tsx
@@ -5,29 +5,32 @@ import { Button, Group } from "@mantine/core";
 import { useEffect, useState } from "react";
 
 function AttendanceButtonWrapper({ onWorkModalOpen, offWorkModalOpen }: any) {
-  const { attendance } = attendanceStore();
-  const [checkOutDisable, setCheckOutDisable] = useState(false);
+  const { myInfo } = attendanceStore();
+
+  const [checkInDone, setCheckInDone] = useState(false);
+  const [checkOutDone, setCheckOutDone] = useState(false);
 
   useEffect(() => {
-    if (!attendance) {
-      setCheckOutDisable(true);
-    } else {
-      setCheckOutDisable(false);
+    if (myInfo?.attendance) {
+      if (myInfo?.attendance.includes("출근")) {
+        setCheckInDone(true);
+      }
+      if (myInfo?.attendance.includes("퇴근")) {
+        setCheckOutDone(true);
+        setCheckInDone(true);
+      } else {
+        setCheckOutDone(false);
+      }
     }
-  }, [attendance]);
+  }, [myInfo]);
 
   return (
     <Group wrap="nowrap" my="sm">
-      <Button fullWidth variant="filled" onClick={onWorkModalOpen}>
-        출근하기
+      <Button fullWidth variant="filled" onClick={onWorkModalOpen} disabled={checkInDone}>
+        {checkInDone ? "출근완료" : "출근하기"}
       </Button>
-      <Button
-        fullWidth
-        variant="filled"
-        disabled={checkOutDisable}
-        onClick={offWorkModalOpen}
-      >
-        퇴근하기
+      <Button fullWidth variant="filled" disabled={checkOutDone} onClick={offWorkModalOpen}>
+        {checkOutDone ? "퇴근완료" : "퇴근하기"}
       </Button>
     </Group>
   );
