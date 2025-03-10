4519ba36794c661b499587bfe6824dc2427edd77
aksel26 <kevinxkim2023@gmail.com>
2025-03-10 18:05:39 +0900

    휴가 상세보기 페이지 개발중

--- src/app/(content)/attendance/vacation/list/page.tsx
+++ src/app/(content)/attendance/vacation/list/page.tsx
@@ -1,8 +1,30 @@
 "use client";
 
 import "@/styles/calendar.css";
-import { Breadcrumbs, Container, Text } from "@mantine/core";
+import IconInfoCircle from "/public/icons/info-circle.svg";
 import { useDisclosure } from "@mantine/hooks";
+import {
+  Badge,
+  Breadcrumbs,
+  Button,
+  Container,
+  Divider,
+  Drawer,
+  FileButton,
+  Grid,
+  GridCol,
+  Group,
+  Indicator,
+  Input,
+  List,
+  Paper,
+  Popover,
+  Select,
+  Stack,
+  Text,
+  ThemeIcon,
+  Title,
+} from "@mantine/core";
 
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
@@ -35,6 +57,90 @@ function page() {
       }}
     >
       <Breadcrumbs mb={"md"}>{items}</Breadcrumbs>
+      <Grid>
+        <GridCol span={{ base: 12, md: 8 }}>
+          <Paper bg={"white"} px="md" py="lg" radius={"lg"} h={"100%"}>
+            <Group justify="space-between" align="center" mb={"xs"}>
+              <Title order={5}>
+                휴가 요약{" "}
+                <Text component="span" c={"dimmed"} fz={"sm"} ml={"xs"}>
+                  2025년 기준
+                </Text>
+              </Title>
+            </Group>
+            <Group gap={"xs"} justify="space-evenly">
+              <Stack gap={4}>
+                <Popover width={"auto"} position="top-start" withArrow shadow="md">
+                  <Popover.Target>
+                    <Group align="center" gap={4}>
+                      <Text fz={"sm"}>총 연차 수</Text>
+                      <ThemeIcon variant="white">
+                        <IconInfoCircle />
+                      </ThemeIcon>
+                    </Group>
+                  </Popover.Target>
+                  <Popover.Dropdown>
+                    <Group>
+                      <Stack gap={4}>
+                        <Text fz={"xs"}>현재날짜</Text>
+                        <Text fz={"xs"}>2024-11-28</Text>
+                      </Stack>
+                      <Stack gap={4}>
+                        <Text fz={"xs"}>입사일</Text>
+                        <Text fz={"xs"}>1999-02-19</Text>
+                      </Stack>
+                      <Stack gap={4}>
+                        <Text fz={"xs"}>근속년수</Text>
+                        <Text fz={"xs"}>25년</Text>
+                      </Stack>
+                      <Stack gap={4}>
+                        <Text fz={"xs"}>누적 연차 수</Text>
+                        <Text fz={"xs"}>10개</Text>
+                      </Stack>
+                    </Group>
+                  </Popover.Dropdown>
+                </Popover>
+                <Text fz={"sm"} ta={"center"}>
+                  <Text fw={900} component="span" fz={"xl"}>
+                    20
+                  </Text>
+                  일
+                </Text>
+              </Stack>
+              <Divider orientation="vertical" />
+              <Stack gap={4}>
+                <Text fz={"sm"}>미승인 요청건</Text>
+                <Text fz={"sm"} ta={"center"}>
+                  <Text fw={900} component="span" fz={"xl"}>
+                    2
+                  </Text>
+                  건
+                </Text>
+              </Stack>
+              <Divider orientation="vertical" />
+              <Stack gap={4}>
+                <Text fz={"sm"}>사용 연차 수</Text>
+                <Text fz={"sm"} ta={"center"}>
+                  <Text fw={900} component="span" fz={"xl"}>
+                    20
+                  </Text>
+                  일
+                </Text>
+              </Stack>
+              <Divider orientation="vertical" />
+              <Stack gap={4}>
+                <Text fz={"sm"}>잔여 연차 수</Text>
+                <Text fz={"sm"} ta={"center"}>
+                  <Text fw={900} component="span" fz={"xl"}>
+                    20
+                  </Text>
+                  일
+                </Text>
+              </Stack>
+            </Group>
+          </Paper>
+        </GridCol>
+      </Grid>
     </Container>
   );
 }
