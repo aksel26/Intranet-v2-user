2713f8c7d8e058f99cab1f27f9769b7f6424c90b
aksel26 <kevinxkim2023@gmail.com>
2025-03-14 16:34:59 +0900

    대시보드 출근내역

--- src/components/Dashboard/attendance/index.tsx
+++ src/components/Dashboard/attendance/index.tsx
@@ -1,12 +1,11 @@
 import * as api from "@/app/api/get/getApi";
-import { Button, Group, Paper, Text, Title } from "@mantine/core";
+import { Badge, Button, Divider, Group, Paper, Stack, Text, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
-import { useRouter } from "next/navigation";
-import ArrowRight from "/public/icons/arrow-right.svg";
 import { isEmpty } from "lodash";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-
+import ArrowRight from "/public/icons/arrow-right.svg";
 const AttendanceAll = () => {
   const router = useRouter();
   const goNotice = () => router.push("/notice");
@@ -18,10 +17,10 @@ const AttendanceAll = () => {
 
   useEffect(() => {
     if (data) {
-      if (isEmpty(data?.data.data.leaveList)) {
+      if (isEmpty(data?.data.data.leaveByType)) {
         setLeaveList(false);
       } else {
-        isEmpty(data?.data.data.leaveList);
+        setLeaveList(data?.data.data.leaveByType);
       }
     }
   }, [data]);
@@ -39,28 +38,30 @@ const AttendanceAll = () => {
           나의 근태 현황 보기
         </Button>
       </Group>
-
-      <Text c={"dimmed"} fz={"sm"} ta={"center"} my={"md"}>
-        근태 내역이 없습니다.
-      </Text>
-
-      {/* {data?.data.data.leaveList.length < 1 ? (
+      {!leaveList ? (
+        <Text c={"dimmed"} fz={"sm"} ta={"center"} my={"md"}>
+          근태 내역이 없습니다.
+        </Text>
       ) : (
         <Stack gap={"sm"} mt={"md"}>
-          {data?.data.data.leaveList.map((leave: any) => (
-            <Group>
-              <Badge size="sm">{leave.leaveType}</Badge>
-              <Group gap={7}>
-                <Text fz={"sm"}>{leave.userName}</Text>
-                <Divider orientation="vertical" />
-                <Text fz={"sm"}>김정순</Text>
-                <Divider orientation="vertical" />
-                <Text fz={"sm"}>김정순</Text>
+          {Object.entries(leaveList).map((list: any) => {
+            return (
+              <Group>
+                <Badge size="sm">{list[0]}</Badge>
+
+                <Group gap={7}>
+                  {list[1].map((name: string, index: number, arr: any) => (
+                    <>
+                      <Text fz={"sm"}>{name}</Text>
+                      {arr.length === index + 1 ? null : <Divider orientation="vertical" />}
+                    </>
+                  ))}
+                </Group>
               </Group>
-            </Group>
-          ))}
+            );
+          })}
         </Stack>
-      )} */}
+      )}
     </Paper>
   );
 };
