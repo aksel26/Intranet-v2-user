26d60cf4b91b8b96567829f48e73b9e5d2bfd1b1
aksel26 <kevinxkim2023@gmail.com>
2025-03-17 11:56:23 +0900

    시간계산 util

--- src/utils/dateFomat.ts
+++ src/utils/dateFomat.ts
@@ -2,5 +2,23 @@ import dayjs from "dayjs";
 
 // 2024-02-01
 export const formatYYYYMMDD = (date: string | null) => {
-return dayjs(date).format('YYYY-MM-DD')
+  return dayjs(date).format("YYYY-MM-DD");
+};
+
+// HH-mm-ss 24시간 형식
+export const formatTime = (date: string | null) => {
+  if (!date) return "시간 정보가 없습니다.";
+  return dayjs(date).format("HH:mm:ss");
+};
+
+export const calculateNumberToTime = (minutes: number) => {
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = Math.floor(minutes % 60);
+  const seconds = 0; // 초는 없으므로 0으로 설정
+
+  return {
+    hours,
+    minutes: remainingMinutes,
+    seconds,
+  };
 };
