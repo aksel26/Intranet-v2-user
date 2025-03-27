24abeb98da71e13697168359b8c5a489048c725c
aksel26 <kevinxkim2023@gmail.com>
2025-03-27 16:36:39 +0900

    type 추가

--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -9,6 +9,9 @@ export interface TMyVacations {
   month: string;
   year: string;
 }
+export interface TApproval extends TMyVacations {
+  user: string;
+}
 
 export interface TWorkHourStats {
   month: string;
