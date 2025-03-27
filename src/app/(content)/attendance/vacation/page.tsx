76e5888dac64268c280205cc2b1fd0ce4c91bd5e
aksel26 <kevinxkim2023@gmail.com>
2025-03-27 16:35:09 +0900

    연차/휴가내역 조회

--- src/app/(content)/attendance/vacation/page.tsx
+++ src/app/(content)/attendance/vacation/page.tsx
@@ -2,21 +2,22 @@
 
 import * as api from "@/app/api/get/getApi";
 import "@/styles/calendar.css";
-import { Badge, Breadcrumbs, Button, Collapse, Container, Divider, Flex, Group, Loader, Paper, Popover, Select, Stack, Text, Title } from "@mantine/core";
+import { Breadcrumbs, Button, Collapse, Container, Flex, Group, Loader, Paper, Select, Stack, Text } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useState } from "react";
 import ArrowDown from "/public/icons/arrow-down.svg";
 import ArrowRight from "/public/icons/arrow-right.svg";
 
+import CancleVacation from "@/components/Attendance/CancleVacation";
 import ConfirnStatus from "@/components/Attendance/ConfirmStatus";
-import Vacation from "@/components/Attendance/Vacation";
+import ToolTipDetailsVacation from "@/components/Attendance/ToolTipDetailsVacation";
 import { TMyVacations } from "@/types/apiTypes";
 import { monthList, yearsList } from "@/utils/dateFomat";
 import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
-import { useRouter } from "next/navigation";
 dayjs.locale("ko");
+
 const items = [{ title: "휴가/연차 상세조회", href: "#" }].map((item, index) => (
   <Text size="lg" fw={600} component="a" key={index}>
     {/* <Anchor href={item.href} key={index}> */}
@@ -25,22 +26,24 @@ const items = [{ title: "휴가/연차 상세조회", href: "#" }].map((item, in
   </Text>
 ));
 
-const CountText = ({ children }: any) => {
-  return <Text size="xs">{children}</Text>;
-};
 function page() {
-  const [opened, { open, close }] = useDisclosure(false);
-
-  const router = useRouter();
+  const [cancelVacationOpened, { open: cancelOpenVacationModal, close: cancelVacationModalClose }] = useDisclosure(false);
 
   const [params, setParams] = useState<TMyVacations>({
     year: dayjs().year().toString(),
     month: (dayjs().month() + 1).toString(),
   });
   const { data, isLoading, isError } = useQuery({ queryKey: ["vacationAll", params], queryFn: () => api.getMyVacations(params) });
-  console.log("🚀 ~ page ~ data:", data);
+  const {
+    data: summary,
+    isLoading: isLoading_summary,
+    isError: isError_summary,
+  } = useQuery({ queryKey: ["vacationSummary", { year: params.year }], queryFn: () => api.getVacationSummary({ year: params.year }) });
+  console.log("🚀 ~ page ~ summary:", summary);
 
   const vacations = data?.data.data;
+  const leaveSummary = summary?.data.data.leaveSummary;
+  const leaveUsageStats = summary?.data.data.leaveUsageStats;
 
   const [openedId, setOpenedId] = useState<number | null>(null);
 
@@ -59,6 +62,11 @@ function page() {
     setParams((params) => ({ ...params, year: e }));
     setYearValue(e);
     setIsActive((prev) => ({ ...prev, yearSelect: false }));
+  };
+  const [currentVacationInfo, setCurrentVacationInfo] = useState();
+  const openVacationModal = (record: any) => {
+    setCurrentVacationInfo(record);
+    cancelOpenVacationModal();
   };
 
   return (
@@ -102,260 +110,102 @@ function page() {
           setIsActive((prev) => ({ ...prev, yearSelect: true }));
         }}
       />
-
       <Flex gap={"md"} wrap={"wrap"}>
-        <Paper bg={"white"} px="lg" py="md" radius={"lg"} h={"100%"}>
-          <Group gap={"xl"}>
-            <Stack gap={4}>
-              <Popover width={"auto"} position="top-start" withArrow shadow="md">
-                <Popover.Target>
-                  <Group align="center" gap={4}>
+        <Paper bg={"white"} px="lg" py="md" radius={"lg"} h={"100%"} miw={300}>
+          {isLoading_summary ? (
+            <Loader color="blue" type="dots" />
+          ) : (
+            <Group gap={"xl"}>
+              <Stack gap={4}>
+                <Text c={"dimmed"} fz={"xs"}>
+                  입사일
+                </Text>
+                <Text fw={600} ta={"center"} fz={"sm"}>
+                  {leaveSummary.joinDate}
+                </Text>
+              </Stack>
+              <Stack gap={4}>
+                <Text c={"dimmed"} fz={"xs"}>
+                  근속년수
+                </Text>
+                <Text fz={"sm"} ta={"center"}>
+                  <Text fw={600} component="span" fz={"md"}>
+                    {leaveSummary.yearsSinceJoin}
+                  </Text>
+                  년
+                </Text>
+              </Stack>
+              {leaveSummary.yearsSinceJoin >= 3 ? null : (
+                <>
+                  <Stack gap={4}>
                     <Text c={"dimmed"} fz={"xs"}>
-                      입사일
+                      만 1년 날짜
                     </Text>
-                    {/* <ThemeIcon variant="white">
-                      <IconInfoCircle />
-                    </ThemeIcon> */}
-                  </Group>
-                </Popover.Target>
-                <Popover.Dropdown>
-                  <Paper bg={"white"} px="md" py="lg" radius={"lg"}>
-                    <Title order={5} mb={"xs"}>
-                      휴가 종류별 사용 일 수
-                    </Title>
-                    <Group gap={"xl"}>
-                      <Stack gap={"xs"} justify="center" align="center">
-                        <Group justify="space-between" w={"100%"}>
-                          <Badge radius={"sm"}>연차</Badge>
-
-                          <CountText>1개</CountText>
-                        </Group>
-                        <Group justify="space-between" w={"100%"}>
-                          <Badge radius={"sm"} size="md">
-                            반차
-                          </Badge>
-
-                          <CountText>1개</CountText>
-                        </Group>
-                        <Group align="center">
-                          <Badge radius={"sm"}>반반차</Badge>
-                          <CountText>1개</CountText>
-                        </Group>
-                      </Stack>
-                      <Divider orientation="vertical" />
-                      <Group gap={"lg"} align="center">
-                        <Stack gap={"xs"}>
-                          <Group justify="space-between" w={"100%"}>
-                            <Badge variant="outline" radius={"sm"}>
-                              조퇴
-                            </Badge>
-
-                            <CountText>1개</CountText>
-                          </Group>
-                          <Group justify="space-between" w={"100%"}>
-                            <Badge variant="outline" radius={"sm"}>
-                              훈련
-                            </Badge>
-
-                            <CountText>1개</CountText>
-                          </Group>
-                          <Group align="center">
-                            <Badge variant="outline" radius={"sm"}>
-                              대체휴무
-                            </Badge>
-                            <CountText>1개</CountText>
-                          </Group>
-                        </Stack>
-                        <Stack gap={"xs"}>
-                          <Group align="center">
-                            <Badge variant="outline" radius={"sm"}>
-                              특별휴가
-                            </Badge>
-                            <CountText>1개</CountText>
-                          </Group>
-                          <Group align="center">
-                            <Badge variant="outline" radius={"sm"}>
-                              경조휴무
-                            </Badge>
-                            <CountText>1개</CountText>
-                          </Group>
-                          <Group align="center">
-                            <Badge variant="outline" radius={"sm"}>
-                              무급휴가
-                            </Badge>
-                            <CountText>1개</CountText>
-                          </Group>
-                        </Stack>
-                      </Group>
-                    </Group>
-                  </Paper>
-                </Popover.Dropdown>
-              </Popover>
-              <Text fw={600} ta={"center"} fz={"sm"}>
-                2022-02-12
-              </Text>
-            </Stack>
-            <Stack gap={4}>
-              <Text c={"dimmed"} fz={"xs"}>
-                근속년수
-              </Text>
-              <Text fz={"sm"} ta={"center"}>
-                <Text fw={600} component="span" fz={"md"}>
-                  2
-                </Text>
-                년
-              </Text>
-            </Stack>
-            <Stack gap={4}>
-              <Text c={"dimmed"} fz={"xs"}>
-                만 1년 날짜
-              </Text>
-              <Text fz={"sm"} ta={"center"}>
-                <Text fw={600} component="span" fz={"md"}>
-                  2
-                </Text>
-                년
-              </Text>
-            </Stack>
-            <Stack gap={4}>
-              <Text c={"dimmed"} fz={"xs"}>
-                중도입사 연차 부여
-              </Text>
-              <Text fz={"sm"} ta={"center"}>
-                <Text fw={600} component="span" fz={"md"}>
-                  7.25
-                </Text>
-                개
-              </Text>
-            </Stack>
-          </Group>
+                    <Text fz={"sm"} fw={600} ta={"center"}>
+                      {leaveSummary.oneYearAfterJoin}
+                    </Text>
+                  </Stack>
+                  <Stack gap={4}>
+                    <Text c={"dimmed"} fz={"xs"}>
+                      중도입사 연차 부여
+                    </Text>
+                    <Text fz={"sm"} ta={"center"}>
+                      <Text fw={600} component="span" fz={"md"}>
+                        {leaveSummary.midJoinReceivedAnnualLeave}
+                      </Text>
+                      개
+                    </Text>
+                  </Stack>
+                </>
+              )}
+            </Group>
+          )}
         </Paper>
 
-        <Paper bg={"white"} px="lg" py="md" h={"100%"} radius={"lg"}>
-          <Group gap={"xl"}>
-            <Stack gap={4}>
-              <Text c={"dimmed"} fz={"xs"}>
-                총 연차 개수
-              </Text>
-              <Text fz={"sm"} ta={"center"}>
-                <Text fw={600} component="span" fz={"md"}>
-                  20
+        <Paper bg={"white"} px="lg" py="md" h={"100%"} radius={"lg"} miw={300} pos={"relative"}>
+          {isLoading_summary ? (
+            <Loader color="blue" type="dots" />
+          ) : (
+            <Group gap={"xl"} justify="space-around" align="end">
+              <Stack gap={4}>
+                <Text c={"dimmed"} fz={"xs"}>
+                  총 연차 개수
                 </Text>
-                일
-              </Text>
-            </Stack>
-            <Stack gap={4}>
-              <Text c={"dimmed"} fz={"xs"}>
-                사용 연차
-              </Text>
-              <Text fz={"sm"} ta={"center"}>
-                <Text fw={600} component="span" fz={"md"}>
-                  20
+                <Text fz={"sm"} ta={"center"}>
+                  <Text fw={600} component="span" fz={"md"}>
+                    {leaveSummary.totalReceivedAnnualLeave}
+                  </Text>
+                  일
                 </Text>
-                일
-              </Text>
-            </Stack>
-            <Stack gap={4}>
-              <Text c={"dimmed"} fz={"xs"}>
-                잔여 연차
-              </Text>
-              <Text fz={"sm"} ta={"center"}>
-                <Text fw={600} component="span" fz={"md"}>
-                  20
-                </Text>
-                일
-              </Text>
-            </Stack>
-          </Group>
-          {/* <Title order={5} mb={"xs"}>
-              휴가 종류별 사용 일 수
-            </Title>
-            <Group gap={"xl"}>
-              <Stack gap={"xs"} justify="center" align="center">
-                <Group justify="space-between" w={"100%"}>
-                  <Badge radius={"sm"}>연차</Badge>
-
-                  <CountText>1개</CountText>
-                </Group>
-                <Group justify="space-between" w={"100%"}>
-                  <Badge radius={"sm"} size="md">
-                    반차
-                  </Badge>
-
-                  <CountText>1개</CountText>
-                </Group>
-                <Group align="center">
-                  <Badge radius={"sm"}>반반차</Badge>
-                  <CountText>1개</CountText>
-                </Group>
               </Stack>
-              <Divider orientation="vertical" />
-              <Group gap={"lg"} align="center">
-                <Stack gap={"xs"}>
-                  <Group justify="space-between" w={"100%"}>
-                    <Badge variant="outline" radius={"sm"}>
-                      조퇴
-                    </Badge>
+              <Stack gap={4}>
+                <Text c={"dimmed"} fz={"xs"}>
+                  사용 연차
+                </Text>
+                <Text fz={"sm"} ta={"center"}>
+                  <Text fw={600} component="span" fz={"md"}>
+                    {leaveSummary.totalAnnualLeaveUsage}
+                  </Text>
+                  일
+                </Text>
+              </Stack>
+              <Stack gap={4}>
+                <Text c={"dimmed"} fz={"xs"}>
+                  잔여 연차
+                </Text>
+                <Text fz={"sm"} ta={"center"}>
+                  <Text fw={600} component="span" fz={"md"}>
+                    {leaveSummary.totalAnnualLeaveBalance}
+                  </Text>
+                  일
+                </Text>
+              </Stack>
 
-                    <CountText>1개</CountText>
-                  </Group>
-                  <Group justify="space-between" w={"100%"}>
-                    <Badge variant="outline" radius={"sm"}>
-                      훈련
-                    </Badge>
-
-                    <CountText>1개</CountText>
-                  </Group>
-                  <Group align="center">
-                    <Badge variant="outline" radius={"sm"}>
-                      대체휴무
-                    </Badge>
-                    <CountText>1개</CountText>
-                  </Group>
-                </Stack>
-                <Stack gap={"xs"}>
-                  <Group align="center">
-                    <Badge variant="outline" radius={"sm"}>
-                      특별휴가
-                    </Badge>
-                    <CountText>1개</CountText>
-                  </Group>
-                  <Group align="center">
-                    <Badge variant="outline" radius={"sm"}>
-                      경조휴무
-                    </Badge>
-                    <CountText>1개</CountText>
-                  </Group>
-                  <Group align="center">
-                    <Badge variant="outline" radius={"sm"}>
-                      무급휴가
-                    </Badge>
-                    <CountText>1개</CountText>
-                  </Group>
-                </Stack>
-              </Group>
-            </Group> */}
+              <ToolTipDetailsVacation details={leaveUsageStats} />
+            </Group>
+          )}
         </Paper>
       </Flex>
-      {/* 
-      <DatePickerInput
-        // label="조회기간"
-        type="range"
-        locale="ko"
-        highlightToday
-        firstDayOfWeek={0}
-        clearable
-        allowSingleDateInRange
-        placeholder="조회일자를 선택해 주세요."
-        miw={180}
-        w={"max-content"}
-        styles={{ input: { letterSpacing: 1, border: "none", paddingLeft: 25 }, section: { justifyContent: "start" } }}
-        valueFormat="YYYY/MM/DD"
-        leftSection={<IconCalendar />}
-        onChange={dateSelect}
-        value={dateValue}
-        mt={"md"}
-      /> */}
 
       <Select
         mt={"lg"}
@@ -397,7 +247,7 @@ function page() {
               vacations?.map((record: any) => {
                 const isOpen = openedId === record.commuteIdx;
                 return (
-                  <Stack key={record.commuteIdx} styles={{ root: { cursor: "pointer" } }}>
+                  <Stack key={record.commuteIdx} gap={2} styles={{ root: { cursor: "pointer" } }}>
                     <Group gap={2} align="center" justify="space-between" wrap="nowrap" onClick={() => setOpenedId(isOpen ? null : record.commuteIdx)}>
                       <div className="flex flex-col">
                         <Text c={"dimmed"} fz={"xs"}>
@@ -437,7 +287,9 @@ function page() {
                           <Text fz={"xs"} c={"dimmed"}>
                             결재자
                           </Text>
-                          <Text fz={"xs"}>{record.confirmPersonName}</Text>
+                          <Text c={!record.confirmPersonName ? "dimmed" : "black"} fz={"xs"}>
+                            {record.confirmPersonName || "결재 승인 전 입니다."}
+                          </Text>
                         </Stack>
 
                         <Stack gap={1}>
@@ -453,7 +305,7 @@ function page() {
                           )}
                         </Stack>
                         {record.confirmYN === "N" ? (
-                          <Button variant="outline" color="red" size="xs">
+                          <Button variant="outline" color="red" size="xs" onClick={() => openVacationModal(record)}>
                             신청취소
                           </Button>
                         ) : (
@@ -470,8 +322,8 @@ function page() {
           </Stack>
         )}
       </Paper>
-
-      <Vacation opened={opened} close={close} />
+      <CancleVacation opened={cancelVacationOpened} close={cancelVacationModalClose} details={currentVacationInfo} />
+      {/* <Vacation opened={opened} close={close} /> */}
     </Container>
   );
 }
