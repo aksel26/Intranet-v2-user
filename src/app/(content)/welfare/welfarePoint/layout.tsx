af283ecb3f7c7abf965d4d28c3c53db0779ebc38
aksel26 <kevinxkim2023@gmail.com>
2025-03-28 16:31:14 +0900

    복지포인트 변경 API 반영

--- src/app/(content)/welfare/welfarePoint/layout.tsx
+++ src/app/(content)/welfare/welfarePoint/layout.tsx
@@ -1,17 +1,19 @@
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import React from "react";
 import * as api from "@/app/api/get/getApi";
+import dayjs from "dayjs";
 
 export default async function layout({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
 
   // const now = {
-  const year = new Date().getFullYear();
-  const month = new Date().getMonth() + 1;
+  const year = dayjs().year().toString();
+
+  const halfYear = Number(year) > 6 ? "H2" : "H1";
   // };
   await queryClient.prefetchQuery({
-    queryKey: ["welfares", { year, month }],
-    queryFn: () => api.getMeals({ year, month }),
+    queryKey: ["welfares", { year, halfYear }],
+    queryFn: () => api.getWelfares({ year, halfYear }),
   });
   return <HydrationBoundary state={dehydrate(queryClient)}>{children}</HydrationBoundary>;
 }
--- src/app/(content)/welfare/welfarePoint/page.tsx
+++ src/app/(content)/welfare/welfarePoint/page.tsx
@@ -1,42 +1,93 @@
 "use client";
 
 import { TopTitle } from "@/components/content/welfare/TopTitle";
-import { UsedList } from "@/components/content/welfare/UsedList";
-import { useCombinedStore } from "@/lib/store/CombinedStore";
 
-import { welfareStateStore } from "@/lib/store/welfareStore";
-import { Container, Flex, Grid, GridCol, Paper, Stack, Title } from "@mantine/core";
+import WelfareInputForm from "@/components/content/welfare/WelfareInputForm";
+import { groupByDate } from "@/utils/welfare/groupByDate";
+import { Container, Grid, GridCol, Group, Paper, Select, Stack, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import * as api from "../../../api/get/getApi";
-import WelfareInputForm from "@/components/content/welfare/WelfareInputForm";
+import { yearsList } from "@/utils/dateFomat";
+import { UsedListWelfare } from "@/components/content/welfare/UsedListWelfare";
 const WelfareMain = () => {
-  const nowMonthYear = dayjs();
-  const [calendarYearMonth, setCalendarYearMonth] = useState({
-    year: nowMonthYear.year(),
-    month: nowMonthYear.month() + 1,
+  const [params, setParams] = useState({
+    year: dayjs().year().toString(),
+    halfYear: Number(dayjs().month() + 1) > 6 ? "H2" : "H1",
   });
 
-  const { data, isLoading, isError } = useQuery({ queryKey: ["welfares", calendarYearMonth], queryFn: () => api.getWelfares(calendarYearMonth) });
+  const { data, isLoading, isError } = useQuery({ queryKey: ["welfares", params], queryFn: () => api.getWelfares(params) });
 
-  const { welfareStore } = useCombinedStore() as { welfareStore: welfareStateStore };
+  const welfares = data && groupByDate(data?.data.data.welfares);
+  const welfareStats = data?.data.data.welfareStats;
 
-  useEffect(() => {
-    if (data) {
-      const result = data?.data.data;
-      welfareStore.setwelfareInfo(result);
-    }
-  }, [data]);
+  const [halfYearValue, setHalfYearValue] = useState<string>(params.halfYear);
+  const [yearValue, setYearValue] = useState<string | null>(dayjs().year().toString());
 
+  const [isActive, setIsActive] = useState({
+    yearSelect: false,
+    halfYearSelect: false,
+  });
+  const selectHalfYear = (e: any) => {
+    setParams((params) => ({ ...params, halfYear: e }));
+    setHalfYearValue(e);
+    setIsActive((prev) => ({ ...prev, halfYearSelect: false }));
+  };
+  const selectYear = (e: any) => {
+    setParams((params) => ({ ...params, year: e }));
+    setYearValue(e);
+    setIsActive((prev) => ({ ...prev, yearSelect: false }));
+  };
   return (
     <Container fluid p={"lg"} style={{ scrollPaddingBottom: "52px", overflowY: "auto", scrollSnapType: "y mandatory" }}>
       <Grid>
         <GridCol span={{ base: 12, md: 8 }}>
-          <Stack>
-            <TopTitle />
-            <UsedList setCalendarYearMonth={setCalendarYearMonth} />
-          </Stack>
+          <TopTitle stats={welfareStats} isLoading={isLoading} />
+          <Group mt={"lg"} mb={"xs"}>
+            <Select
+              w={130}
+              comboboxProps={{
+                withinPortal: false,
+                transitionProps: { transition: "pop", duration: 200 },
+              }}
+              onChange={selectYear}
+              value={yearValue}
+              data={yearsList().map((item) => ({ value: item.toString(), label: `${item}년` }))}
+              styles={{
+                root: { width: "max-content" },
+                input: { background: "transparent", border: "none", fontSize: "var(--mantine-font-size-lg)", fontWeight: 600 },
+              }}
+              dropdownOpened={isActive.yearSelect}
+              onBlur={() => setIsActive((prev) => ({ ...prev, yearSelect: false }))}
+              onClick={() => {
+                setIsActive((prev) => ({ ...prev, yearSelect: true }));
+              }}
+            />
+            <Select
+              w={100}
+              comboboxProps={{
+                withinPortal: false,
+                transitionProps: { transition: "pop", duration: 200 },
+              }}
+              onChange={selectHalfYear}
+              value={halfYearValue}
+              data={[
+                { label: "상반기", value: "H1" },
+                { label: "하반기", value: "H2" },
+              ]}
+              styles={{
+                root: { width: "max-content" },
+                input: { background: "transparent", border: "none", fontSize: "var(--mantine-font-size-lg)", fontWeight: 600 },
+              }}
+              dropdownOpened={isActive.halfYearSelect}
+              onBlur={() => setIsActive((prev) => ({ ...prev, halfYearSelect: false }))}
+              onClick={() => {
+                setIsActive((prev) => ({ ...prev, halfYearSelect: true }));
+              }}
+            />
+          </Group>
+          <UsedListWelfare welfares={welfares} isLoading={isLoading} />
         </GridCol>
         <GridCol span={{ base: 12, md: 4 }} visibleFrom="md">
           <Paper bg={"white"} py="lg" px={"lg"} radius={"lg"}>
