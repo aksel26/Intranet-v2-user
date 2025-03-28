a8809d0f46f6eeb50a2ab23f3a18e44497178050
aksel26 <kevinxkim2023@gmail.com>
2025-03-28 18:48:04 +0900

    식대 Toptitle

---
+++ src/components/content/meal/TopTitleMeal.tsx
@@ -0,0 +1,51 @@
+import { myInfoStore } from "@/lib/store/myInfoStore";
+import { Box, Flex, Group, Loader, Paper, Stack, Text } from "@mantine/core";
+import NumberFlow from "@number-flow/react";
+import dayjs from "dayjs";
+import { usePathname } from "next/navigation";
+
+export const TopTitleMeal = ({ stats, isLoading }: any) => {
+  const { myInfo } = myInfoStore();
+  // const { typeTitle, statsInfo } = useTopTitle({ pathName });
+  if (isLoading)
+    return (
+      <Group justify="center" py={"xl"}>
+        <Loader color="blue" type="dots" />
+      </Group>
+    );
+  return (
+    <Paper bg={"white"} px="md" py="lg" radius={"lg"}>
+      <Stack gap={"md"}>
+        <Box>
+          <Text size="xl" fw={700}>
+            식대
+          </Text>
+          <Text size="sm" fw={500} c={"gray.6"}>
+            {dayjs().format("MM월 DD일 dddd")}
+          </Text>
+        </Box>
+        <Box>
+          <Flex direction={"column"} rowGap={5}>
+            <Text fw={600} c={"blue.9"} fz={"lg"}>
+              {myInfo.userName || ""}
+              <Text c={"gray.9"} component="span" mr={0} fz={"sm"}>
+                님의 잔여 식대는
+              </Text>
+            </Text>
+
+            <Flex align={"center"}>
+              <Text mx={5} component="span" c={"blue.9"} fw={600} fz={"lg"} styles={{ root: { letterSpacing: 0.8 } }}>
+                <NumberFlow
+                  value={stats.mealBalance}
+                  locales="ko-KR" // Intl.NumberFormat locales
+                />
+              </Text>
+              <Text fz={"sm"}>원 입니다</Text>
+            </Flex>
+          </Flex>
+        </Box>
+        {/* <ChartSummary statsInfo={statsInfo} /> */}
+      </Stack>
+    </Paper>
+  );
+};
