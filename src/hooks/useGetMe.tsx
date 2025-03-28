375258698318f937c8ecd068eba9aad78ee871c1
aksel26 <kevinxkim2023@gmail.com>
2025-03-28 14:59:35 +0900

    getMe queryKey 설정

--- src/hooks/useGetMe.tsx
+++ src/hooks/useGetMe.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as api from "@/app/api/get/getApi";
 import { myInfoStore } from "@/lib/store/myInfoStore";
 function useGetMe() {
-  const { data, isLoading, isError } = useQuery({ queryKey: ["me"], queryFn: () => api.getMe() });
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    if (sessionStorage) {
+      const st = sessionStorage.getItem("user");
+      if (st) {
+        const name = JSON.parse(st).userName;
+        setName(name);
+      }
+    }
+  }, []);
+
+  const { data, isLoading, isError } = useQuery({ queryKey: ["me", { name: name }], queryFn: () => api.getMe() });
   const { myInfo, setMyInfo } = myInfoStore();
   useEffect(() => {
     data && setMyInfo(data.data.data);
