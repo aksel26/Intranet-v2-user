2c0cfe43e8d9dccb52fe151ecfa09d29f379719c
aksel26 <kevinxkim2023@gmail.com>
2025-04-04 18:12:07 +0900

    나의 근태 현황보기 이동

--- src/components/Dashboard/attendance/index.tsx
+++ src/components/Dashboard/attendance/index.tsx
@@ -1,15 +1,10 @@
 import * as api from "@/app/api/get/getApi";
-import { Badge, Button, Divider, Group, Paper, Stack, Text, Title } from "@mantine/core";
+import { Badge, Divider, Group, Paper, Stack, Text, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import { isEmpty } from "lodash";
-import { useRouter } from "next/navigation";
 import { Fragment, useEffect, useState } from "react";
-import ArrowRight from "/public/icons/arrow-right.svg";
 const AttendanceAll = ({ date }: { date: Date | string | null }) => {
-  const router = useRouter();
-  const goWorkDetails = () => router.push("/attendance/work");
-
   const { data, isLoading, isError } = useQuery({
     queryKey: ["attendanceAll", { date: dayjs(date).format("YYYY-MM-DD") }],
     queryFn: () => api.getAllAttendance({ date: dayjs(date).format("YYYY-MM-DD") }),
@@ -29,16 +24,11 @@ const AttendanceAll = ({ date }: { date: Date | string | null }) => {
 
   return (
     <Paper p={"lg"} radius={"lg"}>
-      <Group justify="space-between" align="flex-start">
-        <Group>
-          <Title order={5}>직원 근태 현황 </Title>
-          <Text c={"dimmed"} fz={"sm"}>
-            {dayjs(date).format("YYYY-MM-DD")}
-          </Text>
-        </Group>
-        <Button size="xs" variant="subtle" onClick={goWorkDetails} rightSection={<ArrowRight />}>
-          나의 근태 현황 보기
-        </Button>
+      <Group>
+        <Title order={5}>직원 근태 현황 </Title>
+        <Text c={"dimmed"} fz={"sm"}>
+          {dayjs(date).format("YYYY-MM-DD")}
+        </Text>
       </Group>
       {!leaveList ? (
         <Text c={"dimmed"} fz={"sm"} ta={"center"} my={"md"}>
--- src/components/Dashboard/workHourStats/index.tsx
+++ src/components/Dashboard/workHourStats/index.tsx
@@ -1,10 +1,11 @@
 import { CompositeChart } from "@mantine/charts";
-import { Group, Loader, Paper, Title } from "@mantine/core";
+import { ActionIcon, Button, Group, Loader, Paper, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import * as api from "@/app/api/get/getApi";
 import dayjs from "dayjs";
-
+import { useRouter } from "next/navigation";
+import IconDots from "/public/icons/dots.svg";
 const WorkHourStats = ({ dateValue }: any) => {
   const param = { year: dayjs(dateValue).year().toString(), month: (dayjs(dateValue).month() + 1).toString() };
   const { data, isLoading, isError } = useQuery({
@@ -12,12 +13,18 @@ const WorkHourStats = ({ dateValue }: any) => {
     queryFn: () => api.getWorkHourStats(param),
   });
 
+  const router = useRouter();
+  const goWorkDetails = () => router.push("/attendance/work");
+
   return (
     <Paper bg={"white"} px="md" py="lg" radius={"lg"}>
-      <Title order={5} mb={"xs"}>
-        이번달 나의 업무 시간
-      </Title>
+      <Group justify="space-between" align="center" mb={"xs"}>
+        <Title order={5}>이번달 나의 업무 시간</Title>
 
+        <ActionIcon onClick={goWorkDetails} variant="default">
+          <IconDots />
+        </ActionIcon>
+      </Group>
       {isLoading ? (
         <Group justify="center" py={"sm"}>
           <Loader color="blue" type="dots" />
