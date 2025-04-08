88df712d7f01bfcb47812389873e5a385db62dc4
aksel26 <kevinxkim2023@gmail.com>
2025-04-08 19:08:08 +0900

    css

--- src/components/content/meal/detailCard/Holiday.tsx
+++ src/components/content/meal/detailCard/Holiday.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { Button, Flex, Text } from "@mantine/core";
-import ArrowRight from "../../../../../public/icons/arrow-right.svg";
-import IconHoliday from "../../../../../public/icons/holiday.svg";
+import { Button, Text } from "@mantine/core";
 
 export const Holiday = () => {
   return (
-    <Button justify="space-between" fullWidth variant="light" size="lg" radius={"md"} color="lime.3" leftSection={<span />}>
-      <Text size="sm" c={"lime.8"} fw={600} ta={"center"} style={{ alignSelf: "center" }}>
+    <Button fullWidth variant="light" size="lg" radius={"md"} color="lime.3">
+      <Text size="sm" c={"lime.8"} fw={600} ta={"center"}>
         반차 또는 휴무날은 식대가 제공되지 않아요.
       </Text>
     </Button>
