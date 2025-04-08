97c4a5e76b24a321f0ba70e62194bef34080b0b2
aksel26 <kevinxkim2023@gmail.com>
2025-04-08 19:00:00 +0900

    식대 달력 icon

--- src/utils/meal/calendarIcon.ts
+++ src/utils/meal/calendarIcon.ts
@@ -2,5 +2,5 @@ export const calendarIcon = (input: string) => {
   if (!input) return;
   if (input === "근무") return "🍙";
   if (input.includes("반차")) return "🕑";
-  if (input.includes("연차")) return "🌴";
+  if (input.includes("연차") || input.includes("휴무")) return "🌴";
 };
