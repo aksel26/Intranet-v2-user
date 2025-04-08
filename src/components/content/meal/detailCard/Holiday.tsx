f19c38e753c4ce910254296167dd2ed3f2e65ff7
aksel26 <kevinxkim2023@gmail.com>
2025-04-08 18:59:50 +0900

    식대입력 - 휴가일 경우

--- src/components/content/meal/detailCard/Holiday.tsx
+++ src/components/content/meal/detailCard/Holiday.tsx
@@ -4,38 +4,10 @@ import { Button, Flex, Text } from "@mantine/core";
 import ArrowRight from "../../../../../public/icons/arrow-right.svg";
 import IconHoliday from "../../../../../public/icons/holiday.svg";
 
-export const Holiday = ({ toggle }: any) => {
+export const Holiday = () => {
   return (
-    // <Flex justify={"space-between"} columnGap={"xl"} align={"center"} py={5}>
-    //   <Flex wrap="nowrap" align={"center"} columnGap={"xs"}>
-    //     <Flex bg={"blue.0"} justify={"center"} align={"center"} p={4}>
-    //       <IconHoliday width={30} height={25} color={"#fdcb67"} />
-    //     </Flex>
-    //     <Text size="xs" c={"gray.6"} ta={"left"}>
-    //       반차 / 연차일 경우 <br /> 식대가 제공되지 않습니다.
-    //     </Text>
-    //   </Flex>
-    //   <ArrowRight color="gray" width={18} />
-    // </Flex>
-
-    <Button
-      justify="space-between"
-      fullWidth
-      variant="light"
-      size="lg"
-      onClick={toggle}
-      radius={"md"}
-      color="lime.3"
-      rightSection={<ArrowRight color="#2f9e44" />}
-      leftSection={<span />}
-    >
-      <Text
-        size="sm"
-        c={"lime.8"}
-        fw={600}
-        ta={"center"}
-        style={{ alignSelf: "center" }}
-      >
+    <Button justify="space-between" fullWidth variant="light" size="lg" radius={"md"} color="lime.3" leftSection={<span />}>
+      <Text size="sm" c={"lime.8"} fw={600} ta={"center"} style={{ alignSelf: "center" }}>
         반차 또는 휴무날은 식대가 제공되지 않아요.
       </Text>
     </Button>
