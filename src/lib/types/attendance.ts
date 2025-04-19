0efb5fd08d89e0595198e120897d2b36e9ec0c3c
aksel26 <kevinxkim2023@gmail.com>
2025-04-19 16:08:22 +0900

    add type

---
+++ src/lib/types/attendance.ts
@@ -0,0 +1,12 @@
+export interface TAttendance {
+  commuteIdx: number;
+  userIdx: number;
+  userName: string;
+  commuteDate: string;
+  leaveTypeIdx: number;
+  leaveType: string;
+  checkInTime: any;
+  checkOutTime: any;
+  confirmYN: string;
+  createdAt: string;
+}
