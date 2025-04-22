fee08f1d36062e98442d43df952811b9cd726839
aksel26 <kevinxkim2023@gmail.com>
2025-04-22 18:52:03 +0900

    달, 연도, 분기 선택 컴포넌트

---
+++ src/components/Global/dateSelect/MonthSelect.tsx
@@ -0,0 +1,35 @@
+import { monthList } from "@/utils/dateFomat";
+import { Select } from "@mantine/core";
+import dayjs from "dayjs";
+import React, { useState } from "react";
+
+const MonthSelect = ({ setParams, ...props }: any) => {
+  const [isActive, setIsActive] = useState(false);
+  const [monthValue, setMonthValue] = useState<string | null>((dayjs().month() + 1).toString());
+  const selectMonth = (e: any) => {
+    setParams((params: any) => ({ ...params, month: e }));
+    setMonthValue(e);
+    setIsActive(false);
+  };
+  return (
+    <Select
+      {...props}
+      comboboxProps={{
+        withinPortal: false, // 포털 비활성화로 외부 클릭 감지 개선
+        transitionProps: { transition: "pop", duration: 200 },
+        size: "sm",
+      }}
+      onChange={selectMonth}
+      value={monthValue}
+      data={monthList().map((item) => ({ value: item.toString(), label: `${item}월` }))}
+      size="md"
+      variant="unstyled"
+      fw={600}
+      dropdownOpened={isActive}
+      onBlur={() => setIsActive(false)}
+      onClick={() => setIsActive(true)}
+    />
+  );
+};
+
+export default MonthSelect;
---
+++ src/components/Global/dateSelect/YearSelect.tsx
@@ -0,0 +1,35 @@
+import { yearsList } from "@/utils/dateFomat";
+import { Select } from "@mantine/core";
+import dayjs from "dayjs";
+import React, { useState } from "react";
+
+const YearSelect = ({ setParams, ...props }: any) => {
+  const [isActive, setIsActive] = useState(false);
+  const [yearValue, setYearValue] = useState<string | null>(dayjs().year().toString());
+  const selectYear = (e: any) => {
+    setParams((params: any) => ({ ...params, year: e }));
+    setYearValue(e);
+    setIsActive(false);
+  };
+  return (
+    <Select
+      {...props}
+      comboboxProps={{
+        withinPortal: false, // 포털 비활성화로 외부 클릭 감지 개선
+        transitionProps: { transition: "pop", duration: 200 },
+        size: "sm",
+      }}
+      onChange={selectYear}
+      value={yearValue}
+      data={yearsList().map((item) => ({ value: item.toString(), label: `${item}년` }))}
+      size="md"
+      variant="unstyled"
+      fw={600}
+      dropdownOpened={isActive}
+      onBlur={() => setIsActive(false)}
+      onClick={() => setIsActive(true)}
+    />
+  );
+};
+
+export default YearSelect;
