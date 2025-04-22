ee8166b02a8723a4af218c295d505d18a8744e73
aksel26 <kevinxkim2023@gmail.com>
2025-04-22 14:37:46 +0900

    로그인화면 css

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import notification from "@/components/GNB/Notification";
-import { Box, Button, Checkbox, Group, PasswordInput, Space, TextInput } from "@mantine/core";
+import { Box, Button, Checkbox, Group, Image, PasswordInput, Space, Stack, Text, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-
+import NextImage from "next/image";
 import { useRouter } from "next/navigation";
-
+import myImage from "/public/images/ACG_LOGO_GRAY.png";
+import { IconAbc, IconLock } from "@tabler/icons-react";
 export default function Login() {
   const router = useRouter();
 
@@ -56,20 +57,50 @@ export default function Login() {
       });
   };
   return (
-    <Box w={"80%"}>
+    <Box w={"70%"}>
+      <Stack justify="center" mb="md" align="center">
+        <Image component={NextImage} src={myImage} alt="CI" w={"5rem"} h={"1rem"} />
+        <Text fz={{ base: "sm", xs: "sm", sm: "sm", md: "md" }} variant="gradient" gradient={{ from: "blue", to: "pink", deg: 90 }} fw={700}>
+          Valid Approach, Valuable People
+        </Text>
+      </Stack>
       <form onSubmit={form.onSubmit((values) => submit(values))}>
-        <TextInput size="md" withAsterisk label="아이디" placeholder="아이디를 입력해 주세요." key={form.key("id")} {...form.getInputProps("id")} />
+        <TextInput
+          size="md"
+          withAsterisk
+          leftSection={<IconAbc strokeWidth={1.2} />}
+          placeholder="아이디를 입력해 주세요."
+          key={form.key("id")}
+          {...form.getInputProps("id")}
+        />
         <Space h="md" />
+        <PasswordInput
+          leftSection={<IconLock strokeWidth={1.2} />}
+          placeholder="비밀번호를 입력해 주세요."
+          key={form.key("password")}
+          {...form.getInputProps("password")}
+          size="md"
+        />
 
-        <PasswordInput label="비밀번호" placeholder="비밀번호를 입력해 주세요." key={form.key("password")} {...form.getInputProps("password")} size="md" />
-
-        <Checkbox mt="md" label="아이디 기억하기" key={form.key("saveId")} {...form.getInputProps("saveId", { type: "checkbox" })} />
-        <Space h="md" />
         <Group justify="center" mt="md">
           <Button fullWidth type="submit">
             로그인
           </Button>
         </Group>
+        <Group justify="end">
+          <Checkbox
+            mt="md"
+            size="xs"
+            radius={"sm"}
+            labelPosition="left"
+            label="아이디 기억하기"
+            key={form.key("saveId")}
+            {...form.getInputProps("saveId", { type: "checkbox" })}
+          />
+        </Group>
+        <Text c={"dimmed"} ta={"right"} fz={"xs"} mt={"xs"}>
+          계정 생성은 P&C팀에게 문의해 주세요.
+        </Text>
       </form>
     </Box>
   );
