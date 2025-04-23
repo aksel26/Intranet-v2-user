f370ba0c47560621a66a49d8229dc8f038f106e8
aksel26 <kevinxkim2023@gmail.com>
2025-04-23 14:01:57 +0900

    생일자 부분 confetti 적용

--- src/components/Dashboard/noticeBirth/birth/index.tsx
+++ src/components/Dashboard/noticeBirth/birth/index.tsx
@@ -1,35 +1,46 @@
-import React from "react";
 import * as api from "@/app/api/get/getApi";
+import EmptyView from "@/components/Global/view/EmptyView";
+import ErrorView from "@/components/Global/view/ErrorView";
+import LoadingView from "@/components/Global/view/LoadingView";
+import { Group, Stack, Text } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
-import { Group, Loader, Stack, Text } from "@mantine/core";
 import dayjs from "dayjs";
-const Birth = ({ month }: { month: string }) => {
+import Fireworks from "react-canvas-confetti/dist/presets/realistic";
+const Birth = ({ month, activeTab }: { activeTab: string | null; month: string }) => {
   const { data, isLoading, isError } = useQuery({ queryKey: ["notices", { month: month }], queryFn: () => api.getBirth({ month }) });
   const birth = data?.data.data;
-  return (
-    <Stack gap={"md"}>
-      {isLoading ? (
-        <Loader size={"sm"} />
-      ) : birth.length === 0 ? (
-        <Text ta={"center"} fz={"xs"} my={"md"} c={"gray.6"}>
-          생일자가 없습니다.
-        </Text>
-      ) : (
-        birth.map((item: any) => (
-          <Group key={item.userIdx}>
-            <Text fz={"sm"} w={130}>
-              🎉 {item.userName}
-              <Text component="span" ml={"xs"} fz={"sm"}>
-                {item.gradeName}
-              </Text>
-            </Text>
 
-            <Text fz={"sm"}>{dayjs(item.userBirth).format("MM월 DD일")}</Text>
+  const renderContent = () => {
+    if (isLoading) {
+      return <LoadingView />;
+    }
+    if (isError) {
+      return <ErrorView>생일자 인원을 불러오는 중 문제가 발생하였습니다.</ErrorView>;
+    }
+    if (birth.length === 0) {
+      return <EmptyView />;
+    }
+
+    return (
+      <Stack gap={"md"} pos={"relative"}>
+        {birth.map((item: any) => (
+          <Group key={item.userIdx} ml={"xs"}>
+            <Text fz={"sm"}>🎉</Text>
+            <Text fz={"sm"}>{item.userName}</Text>
+            <Text fz={"sm"}>{item.gradeName}</Text>
+            <Text c="dimmed" fz={"sm"}>
+              {dayjs(item.userBirth).format("MM월 DD일")}
+            </Text>
           </Group>
-        ))
-      )}
-    </Stack>
-  );
+        ))}
+        {activeTab === "birth" && (
+          <Fireworks autorun={{ speed: 0.2 }} style={{ position: "absolute", width: "100%", height: 140, right: 0, top: -80, zIndex: 0 }} />
+        )}
+      </Stack>
+    );
+  };
+
+  return <>{renderContent()}</>;
 };
 
 export default Birth;
--- src/components/Dashboard/noticeBirth/index.tsx
+++ src/components/Dashboard/noticeBirth/index.tsx
@@ -12,25 +12,34 @@ const NoticeBirth = () => {
   const [activeTab, setActiveTab] = useState<string | null>("notice");
   const router = useRouter();
   const goNotice = () => router.push("/notice");
+
   return (
     <Paper p={"lg"} radius={"lg"} className={activeTab === "birth" ? "bg-gradient-to-r from-yellow-100 to-red-100" : ""}>
       <Tabs value={activeTab} onChange={setActiveTab} variant="pills" radius={"lg"}>
         <Tabs.List justify="space-between">
-          <Group>
+          <Group styles={{ root: { zIndex: 1 } }}>
             <Tabs.Tab value="notice">공지사항</Tabs.Tab>
-            <Tabs.Tab value="birth">생일자</Tabs.Tab>
+            <Tabs.Tab
+              bg={activeTab === "birth" ? "pink.1" : "transparent"}
+              c={activeTab === "birth" ? "pink.4" : "black"}
+              fw={activeTab === "birth" ? 600 : 400}
+              value="birth"
+            >
+              생일자
+            </Tabs.Tab>
           </Group>
-
-          <Button size="xs" variant="subtle" onClick={goNotice} rightSection={<IconChevronRight size={18} strokeWidth={1.2} />}>
-            더보기
-          </Button>
+          {activeTab === "notice" && (
+            <Button size="xs" variant="subtle" onClick={goNotice} rightSection={<IconChevronRight size={18} strokeWidth={1.2} />}>
+              더보기
+            </Button>
+          )}
         </Tabs.List>
 
         <Tabs.Panel value="notice" pt={"md"}>
           <Notice />
         </Tabs.Panel>
         <Tabs.Panel value="birth" pt={"md"}>
-          <Birth month={(dayjs(dateValue).month() + 1).toString()} />
+          <Birth activeTab={activeTab} month={(dayjs(dateValue).month() + 1).toString()} />
         </Tabs.Panel>
       </Tabs>
     </Paper>
