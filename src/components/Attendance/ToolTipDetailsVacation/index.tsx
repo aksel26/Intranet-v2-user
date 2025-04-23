08abf97d1f7ff7e1c6e4a1813c2ada3cb1ca1603
aksel26 <kevinxkim2023@gmail.com>
2025-04-23 11:08:27 +0900

    휴가 사용현황 상세 예외처리 & css

--- src/components/Attendance/ToolTipDetailsVacation/index.tsx
+++ src/components/Attendance/ToolTipDetailsVacation/index.tsx
@@ -6,12 +6,13 @@ const CountText = ({ children }: any) => {
 };
 const LabelText = ({ children }: any) => {
   return (
-    <Text size="xs" w={70}>
+    <Text size="xs" w={70} ta={"left"}>
       {children}
     </Text>
   );
 };
-const ToolTipDetailsVacation = ({ details }: any) => {
+const ToolTipDetailsVacation = ({ details, children }: any) => {
+  if (!details) return null;
   const {
     alternativeLeaveUsage,
     familyEventLeaveUsage,
@@ -28,51 +29,47 @@ const ToolTipDetailsVacation = ({ details }: any) => {
 
   return (
     <Popover width={"auto"} position="bottom-start" withArrow shadow="md">
-      <Popover.Target>
-        <ActionIcon size="compact-xs" variant="subtle" radius={"xs"}>
-          <IconDots />
-        </ActionIcon>
-      </Popover.Target>
+      <Popover.Target>{children}</Popover.Target>
       <Popover.Dropdown py={"md"}>
         <Paper bg={"white"} radius={"lg"}>
           <Title order={6} mb={"xs"}>
             휴가/연차 유형별 사용 현황
           </Title>
           <Stack gap={"md"}>
-            <Group align="center" justify="space-around">
+            <Group align="center" justify="space-between">
               <LabelText>연차</LabelText>
               <CountText>{fullLeaveUsage}</CountText>
             </Group>
-            <Group align="center" justify="space-around">
+            <Group align="center" justify="space-between">
               <LabelText>반차</LabelText>
               <CountText>{halfLeaveUsage}</CountText>
             </Group>
-            <Group align="center" justify="space-around">
+            <Group align="center" justify="space-between">
               <LabelText>반반차</LabelText>
               <CountText>{quarterLeaveUsage}</CountText>
             </Group>
             <Divider />
-            <Group align="center" justify="space-around">
+            <Group align="center" justify="space-between">
               <LabelText>보건휴가</LabelText>
               <CountText>{healthLeaveUsage}</CountText>
             </Group>
-            <Group align="center" justify="space-around">
+            <Group align="center" justify="space-between">
               <LabelText>특별 휴무</LabelText>
               <CountText>{`${specialLeaveUsage} / ${totalReceivedSpecialLeave}`}</CountText>
             </Group>
-            <Group align="center" justify="space-around">
+            <Group align="center" justify="space-between">
               <LabelText>경조 휴무</LabelText>
               <CountText>{familyEventLeaveUsage}</CountText>
             </Group>
-            <Group align="center" justify="space-around">
+            <Group align="center" justify="space-between">
               <LabelText>병가</LabelText>
               <CountText>{sickLeaveUsage}</CountText>
             </Group>
-            <Group align="center" justify="space-around">
+            <Group align="center" justify="space-between">
               <LabelText>대체 휴무</LabelText>
               <CountText>{`${alternativeLeaveUsage} / ${totalReceivedAlternativeLeave}`}</CountText>
             </Group>
-            <Group align="center" justify="space-around">
+            <Group align="center" justify="space-between">
               <LabelText>훈련</LabelText>
               <CountText>{trainingLeaveUsage}</CountText>
             </Group>
