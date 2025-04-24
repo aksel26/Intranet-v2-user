8e1a226c7ef16a947dbfbf108948675ac4e14b66
aksel26 <kevinxkim2023@gmail.com>
2025-04-24 15:24:14 +0900

    Container 공통 컴포넌트 적용

--- src/app/(content)/myInfo/page.tsx
+++ src/app/(content)/myInfo/page.tsx
@@ -1,11 +1,12 @@
 "use client";
+import PageContainer from "@/components/Global/container";
 import UpdateBaseInfo from "@/components/myInfo/baseInfo";
 import UpdatePassword from "@/components/myInfo/password";
-import { Container, Text } from "@mantine/core";
+import { Text } from "@mantine/core";
 
 const MyInfo = () => {
   return (
-    <Container fluid p={"lg"} style={{ scrollPaddingBottom: "52px", overflowY: "auto", scrollSnapType: "y mandatory" }}>
+    <PageContainer>
       <Text size="lg" fw={600} component="a">
         내 정보 수정
       </Text>
@@ -13,7 +14,7 @@ const MyInfo = () => {
       <UpdateBaseInfo />
 
       <UpdatePassword />
-    </Container>
+    </PageContainer>
   );
 };
 
