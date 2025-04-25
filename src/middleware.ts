88525f674e460325cf7b6bd8f5ea1c15c6d3d8ee
aksel26 <kevinxkim2023@gmail.com>
2025-04-25 17:54:56 +0900

    react-query middleware, interceptor

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  if (!token && req.nextUrl.pathname.startsWith("/main")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/main/:path*"],
+};
