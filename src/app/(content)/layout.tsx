32e05eaae9865ea043952a3553189f57f4ce6a14
aksel26 <kevinxkim2023@gmail.com>
2025-04-26 23:07:11 +0900

    출퇴근 모달 userInfoCard로 이동

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -11,20 +11,25 @@ import React from "react";
 import myImage from "/public/images/ACG_LOGO_GRAY.png";
 
 import CheckOutWrapper from "@/components/Attendance/CheckOutWrapper";
-import AttendanceButtonWrapper from "@/components/Navbar/AttendanceButtonWrapper";
-import Vacation from "@/components/Navbar/attendanceSummary/Vacation";
-import Work from "@/components/Navbar/attendanceSummary/Work";
+import Vacation from "@/components/Navbar/attendanceInfo/Vacation";
+import Work from "@/components/Navbar/attendanceInfo/Work";
 import NavMenu from "@/components/Navbar/menu";
 import UserInfoCard from "@/components/Navbar/userInfoCard";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
 import { useNavStore } from "@/lib/store/toggleStore";
+import { useGetQuery } from "@/lib/api/queryHooks";
+import AttendanceInfo from "@/components/Navbar/attendanceInfo";
 
 const CheckIn = React.lazy(() => import("@/components/Attendance/CheckIn"));
 
 dayjs.locale("ko");
 
-export default function ContentLayout({ children }: { children: React.ReactNode }) {
+export default function ContentLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const pinned = useHeadroom({ fixedAt: 60 });
   // const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
@@ -32,11 +37,16 @@ export default function ContentLayout({ children }: { children: React.ReactNode 
   const mobileOpened = useNavStore((state) => state.mobileOpened);
   const toggleMobile = useNavStore((state) => state.toggleMobile);
 
-  const { myInfo, isLoading, isError } = useGetMe();
+  // const { myInfo, isLoading, isError } = useGetMe();
 
-  const { mutate: logout, isError: isError_logout, isSuccess: isSuccess_logout } = useLogout();
-  const [onWorkTimeOpened, { open: onWorkModalOpen, close: onWorkModalClose }] = useDisclosure(false);
-  const [offWorkTimeOpened, { open: offWorkModalOpen, close: offWorkModalClose }] = useDisclosure(false);
+  // const { data, isLoading, error } = useGetQuery("me", "/users/me");
+  // console.log("data: ", data);
+
+  const {
+    mutate: logout,
+    isError: isError_logout,
+    isSuccess: isSuccess_logout,
+  } = useLogout();
 
   const router = useRouter();
   const handleLogout = async () => {
@@ -82,18 +92,45 @@ export default function ContentLayout({ children }: { children: React.ReactNode 
     >
       <AppShell.Header withBorder={false}>
         <Group h={"100%"} px={"sm"}>
-          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+          <Burger
+            opened={mobileOpened}
+            onClick={toggleMobile}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Burger
+            opened={desktopOpened}
+            onClick={toggleDesktop}
+            visibleFrom="sm"
+            size="sm"
+          />
         </Group>
       </AppShell.Header>
-      <AppShell.Main styles={{ main: { background: "oklch(0.985 0.002 247.839)" } }} pt={`calc(${rem(50)}`}>
+      <AppShell.Main
+        styles={{ main: { background: "oklch(0.985 0.002 247.839)" } }}
+        pt={`calc(${rem(50)}`}
+      >
         {children}
       </AppShell.Main>
       <AppShell.Navbar p="md" withBorder={false}>
         <Group justify="space-between" mb={"lg"}>
           <Group>
-            <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-            <Image onClick={clickLogo} component={NextImage} src={myImage} alt="My image" fit="contain" h={20} w={80} style={{ cursor: "pointer" }} />
+            <Burger
+              opened={mobileOpened}
+              onClick={toggleMobile}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            <Image
+              onClick={clickLogo}
+              component={NextImage}
+              src={myImage}
+              alt="My image"
+              fit="contain"
+              h={20}
+              w={80}
+              style={{ cursor: "pointer" }}
+            />
           </Group>
 
           <Button size="xs" variant="light" onClick={handleLogout}>
@@ -101,15 +138,11 @@ export default function ContentLayout({ children }: { children: React.ReactNode 
           </Button>
         </Group>
 
-        {/* <UserInfoCard myInfo={myInfo} isLoading={isLoading} /> */}
+        <UserInfoCard />
 
-        {/* {myInfo?.attendance === "연차" ? <Vacation /> : <Work myInfo={myInfo} />}
-        <AttendanceButtonWrapper onWorkModalOpen={onWorkModalOpen} offWorkModalOpen={offWorkModalOpen} /> */}
+        <AttendanceInfo />
         <NavMenu />
       </AppShell.Navbar>
-
-      {/* <CheckIn myInfo={myInfo} onWorkModalClose={onWorkModalClose} onWorkTimeOpened={onWorkTimeOpened} />
-      <CheckOutWrapper myInfo={myInfo} offWorkModalClose={offWorkModalClose} offWorkTimeOpened={offWorkTimeOpened} /> */}
     </AppShell>
   );
 }
