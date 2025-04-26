b9c1002fee6e0e665ee6aaa0c063f4bda26e6554
aksel26 <kevinxkim2023@gmail.com>
2025-04-26 23:08:13 +0900

    myInfoStore

--- src/lib/types/myInfo.ts
+++ src/lib/types/myInfo.ts
@@ -1,17 +1,18 @@
-export type TMyInfo = {
-  userIdx: number | null;
-  userName: string | null;
-  userGender: string | null;
-  userCell: string | null;
-  userEmail: string | null;
-  userBirth: string | null;
-  userAddress: string | null;
-  joinDate: string | null;
-  hqName: string | null;
-  teamName: string | null;
-  gradeName: string | null;
-  adminRole: string | null;
-  checkInTime: string | null;
-  attendance: string | null;
-  leaveTypeIdx: number | null;
-};
+export interface TMyInfo {
+  userIdx: number;
+  userName: string;
+  userGender: string;
+  userCell: string;
+  userEmail: string;
+  userBirth: string;
+  userAddress: string;
+  joinDate: string;
+  hqName: string;
+  teamName: string;
+  gradeName: string;
+  adminRole: string;
+  checkInTime: any;
+  attendance: any;
+  workingMinutes: any;
+  leaveTypeIdx: any;
+}
