9687a2a70a0f9c8ea8457ff952328d7443257f03
aksel26 <kevinxkim2023@gmail.com>
2025-04-26 23:21:29 +0900

    axios interceptor 변수명 수정

--- src/app/api/post/postApi.ts
+++ src/app/api/post/postApi.ts
@@ -1,3 +1,4 @@
+import apiClient from "@/lib/axios";
 import axios from "axios";
 
 const postApi = axios.create({
@@ -27,32 +28,47 @@ export const logout = () => postApi.post(`/logout`);
 export const submitForm = (values: any) => postApi.post(`/users/qna`, values);
 
 export const submitMeal = (values: any) => postApi.post(`/users/meals`, values);
-export const submitWelfare = (values: any) => postApi.post(`/users/welfares`, values);
-export const submitActivity = (values: any) => postApi.post(`/users/activities`, values);
-export const submitUpdateWelfare = (values: any) => postApi.put(`/users/welfares/${values.queryParams}`, values.body);
-export const submitUpdateActivity = (values: any) => postApi.put(`/users/activities/${values.queryParams}`, values.body);
+export const submitWelfare = (values: any) =>
+  postApi.post(`/users/welfares`, values);
+export const submitActivity = (values: any) =>
+  postApi.post(`/users/activities`, values);
+export const submitUpdateWelfare = (values: any) =>
+  postApi.put(`/users/welfares/${values.queryParams}`, values.body);
+export const submitUpdateActivity = (values: any) =>
+  postApi.put(`/users/activities/${values.queryParams}`, values.body);
 
 // 내 비밀번호 변경 API
-export const changePassword = (values: any) => postApi.patch(`/users/me/password`, values);
+export const changePassword = (values: any) =>
+  postApi.patch(`/users/me/password`, values);
 // 로그인한 사용자 기본정보 수정 API
 export const changeMyInfo = (values: any) => postApi.put(`/users/me`, values);
 
-export const deleteMeal = (values: any) => postApi.delete(`/users/meals/${values}`);
-export const deleteWelfare = (values: any) => postApi.delete(`/users/welfares/${values}`);
-export const deleteActivity = (values: any) => postApi.delete(`/users/activities/${values}`);
-export const deleteQna = (values: any) => postApi.delete(`/users/qna`, { data: values });
+export const deleteMeal = (values: any) =>
+  postApi.delete(`/users/meals/${values}`);
+export const deleteWelfare = (values: any) =>
+  postApi.delete(`/users/welfares/${values}`);
+export const deleteActivity = (values: any) =>
+  postApi.delete(`/users/activities/${values}`);
+export const deleteQna = (values: any) =>
+  postApi.delete(`/users/qna`, { data: values });
 
 // 점심조 뽑기 API
 
-export const assignLunchGroup = (values: any) => postApi.post(`/users/playground/lunch-group`);
+export const assignLunchGroup = (values: any) =>
+  postApi.post(`/users/playground/lunch-group`);
 
 // 출근 찍기 API
 
-export const checkIn = (values: any) => postApi.post(`/users/intranet/check-in`, values);
+export const checkIn = (values: any) =>
+  apiClient.post(`/users/intranet/check-in`, values);
 
-export const checkOut = (values: any) => postApi.put(`/users/intranet/check-out`, values);
+export const checkOut = (values: any) =>
+  apiClient.put(`/users/intranet/check-out`, values);
 
-export const leave = (values: any) => postApi.post(`/users/intranet/leave`, values, { headers: { "Content-Type": "multipart/form-data" } });
+export const leave = (values: any) =>
+  postApi.post(`/users/intranet/leave`, values, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
 
 // export const getWelfares = ({ year, month }: TWelfaresParams) => getApi.get(`/users/welfares`, { params: { year, month } });
 // export const getActivities = ({ year, month }: TWelfaresParams) => getApi.get(`/users/activities`, { params: { year, month } });
@@ -61,11 +77,21 @@ export const leave = (values: any) => postApi.post(`/users/intranet/leave`, valu
 
 // 사용자 개인 휴가 이미지 수정 API
 export const leaveAttachment = (values: any) =>
-  postApi.patch(`/users/intranet/leave/${values.commuteIdx}/image`, { leaveImage: values.leaveImage }, { headers: { "Content-Type": "multipart/form-data" } });
+  postApi.patch(
+    `/users/intranet/leave/${values.commuteIdx}/image`,
+    { leaveImage: values.leaveImage },
+    { headers: { "Content-Type": "multipart/form-data" } }
+  );
 
 // 사용자 개인 휴가 삭제 API
 
-export const deleteVacation = (values: any) => postApi.delete(`/users/intranet/leave/${values.commuteIdx}`, { data: values });
+export const deleteVacation = (values: any) =>
+  postApi.delete(`/users/intranet/leave/${values.commuteIdx}`, {
+    data: values,
+  });
 
 // 휴가 승인/반려하기 API
-export const approveVacation = (values: any) => postApi.patch(`/users/intranet/approval/${values.commuteIdx}`, { confirmYN: values.confirmYN });
+export const approveVacation = (values: any) =>
+  postApi.patch(`/users/intranet/approval/${values.commuteIdx}`, {
+    confirmYN: values.confirmYN,
+  });
