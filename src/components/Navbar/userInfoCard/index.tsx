57d9f47845ba6cb9c8b570bb3db9ad9d86f5f766
aksel26 <kevinxkim2023@gmail.com>
2025-04-26 23:04:20 +0900

    userInfoCard code splitting

--- src/components/Navbar/userInfoCard/index.tsx
+++ src/components/Navbar/userInfoCard/index.tsx
@@ -1,15 +1,26 @@
+import { getMe } from "@/app/api/get/getApi";
 import { ErrorViewSmall } from "@/components/Global/view/ErrorView";
+import LoadingView from "@/components/Global/view/LoadingView";
+import { myInfoStore } from "@/lib/store/myInfoStore";
 import { Box, Flex, Group, Skeleton, Stack, Text } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 
-type TMyInfo = {
-  myInfo: any;
-  isLoading: boolean;
-  isError: boolean;
-};
+const UserInfoCard = () => {
+  const { setMyInfo } = myInfoStore();
 
-const UserInfoCard = ({ myInfo, isLoading, isError }: TMyInfo) => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["me"],
+    queryFn: () => getMe(),
+  });
+
+  const myInfo = data?.data.data;
+
+  useEffect(() => {
+    setMyInfo(data?.data.data);
+  }, [data]);
+
   const getDDayCount = useCallback((baseDate: string | null) => {
     const today = dayjs();
     const target = dayjs(baseDate);
@@ -19,13 +30,26 @@ const UserInfoCard = ({ myInfo, isLoading, isError }: TMyInfo) => {
   }, []);
 
   const renderContent = () => {
-    if (isError) return <ErrorViewSmall>유저 정보를 불러오는 중 문제가 발생했습니다.</ErrorViewSmall>;
-    if (myInfo === null) return null;
-    const { userName, gradeName, hqName, teamName, userEmail, joinDate } = myInfo;
+    if (isError)
+      return (
+        <ErrorViewSmall>
+          유저 정보를 불러오는 중 문제가 발생했습니다.
+        </ErrorViewSmall>
+      );
+
+    if (isLoading) return <LoadingView />;
+    const { userName, gradeName, hqName, teamName, userEmail, joinDate } =
+      myInfo;
+
     return (
       <Skeleton visible={isLoading}>
         <Stack w={"100%"} bg={"primary.0"} align={"center"} mih={100} p={"md"}>
-          <Flex direction={"column"} w={"100%"} columnGap={"xl"} style={{ position: "relative", borderRadius: 7 }}>
+          <Flex
+            direction={"column"}
+            w={"100%"}
+            columnGap={"xl"}
+            style={{ position: "relative", borderRadius: 7 }}
+          >
             <Flex direction={"column"} rowGap={"md"} w={"100%"}>
               <Box>
                 <Text fz={"lg"} fw={600} c={"primary.9"}>
