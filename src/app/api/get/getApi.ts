fefb0197a51bc3a22b16be0a5d61bf4ea69d2780
aksel26 <kevinxkim2023@gmail.com>
2025-05-23 17:48:06 +0900

    먼슬리 음료 api param

--- src/app/api/get/getApi.ts
+++ src/app/api/get/getApi.ts
@@ -51,4 +51,4 @@ export const getAllAttendanceStaff = (params: TYearMonth) => apiClient.get(`/use
 export const searchNotice = (params: TSearchNotice) => apiClient.get(`/users/notices`, { params: params });
 
 // 사용자 먼슬리 음료 현황 API
-export const monthlyDrink = () => apiClient.get(`/users/playground/monthly-baverage`);
+export const monthlyDrink = ({ month }: { month: string }) => apiClient.get(`/users/playground/monthly-baverage`, { params: { month: month } });
