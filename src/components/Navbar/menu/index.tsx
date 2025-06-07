1109f61bcb7adf7762d96c0d93bde1443e511cf2
aksel26 <kevinxkim2023@gmail.com>
2025-06-08 00:18:58 +0900

    key 중복

--- src/components/Navbar/menu/index.tsx
+++ src/components/Navbar/menu/index.tsx
@@ -14,32 +14,42 @@ interface NavItemWithIndicatorProps {
 }
 
 // Indicator가 있는 NavLink 컴포넌트를 별도로 분리
-const NavItemWithIndicator = memo(({ child, index, hasNew, clickMenu }: NavItemWithIndicatorProps) => (
-  <NavLink
-    pos="relative"
-    onClick={clickMenu}
-    key={`${child.label}-${index}`}
-    component={Link}
-    href={child.href}
-    label={
-      <Group>
-        <Text fz="sm">{child.label}</Text>
-        {hasNew && (
-          <Badge size="xs" variant="light">
-            New
-          </Badge>
-        )}
-      </Group>
-    }
-  />
-));
+const NavItemWithIndicator = memo(
+  ({ child, index, hasNew, clickMenu }: NavItemWithIndicatorProps) => (
+    <NavLink
+      pos="relative"
+      onClick={clickMenu}
+      key={`${child.label}-${index}`}
+      component={Link}
+      href={child.href}
+      label={
+        <Group>
+          <Text fz="sm">{child.label}</Text>
+          {hasNew && (
+            <Badge size="xs" variant="light">
+              New
+            </Badge>
+          )}
+        </Group>
+      }
+    />
+  )
+);
 
 NavItemWithIndicator.displayName = "NavItemWithIndicator";
 
 // 일반 NavLink 컴포넌트
-const RegularNavItem = memo(({ child, index, clickMenu }: Omit<NavItemWithIndicatorProps, "hasNew">) => (
-  <NavLink onClick={clickMenu} key={`${child.label}-${index}`} component={Link} href={child.href} label={child.label} />
-));
+const RegularNavItem = memo(
+  ({ child, index, clickMenu }: Omit<NavItemWithIndicatorProps, "hasNew">) => (
+    <NavLink
+      onClick={clickMenu}
+      key={`${child.label}-${index}`}
+      component={Link}
+      href={child.href}
+      label={child.label}
+    />
+  )
+);
 
 RegularNavItem.displayName = "RegularNavItem";
 
@@ -48,11 +58,34 @@ const NavItem = memo(({ item, clickMenu, hasNew }: NavItemProps) => {
     (child: any, index: number) => {
       switch (child.label) {
         case "결재/승인":
-          return <NavItemWithIndicator child={child} index={index} hasNew={hasNew.approval} clickMenu={clickMenu} />;
+          return (
+            <NavItemWithIndicator
+              key={`${child.label}-${index}`}
+              child={child}
+              index={index}
+              hasNew={hasNew.approval}
+              clickMenu={clickMenu}
+            />
+          );
         case "공지/일정":
-          return <NavItemWithIndicator child={child} index={index} hasNew={hasNew.notice} clickMenu={clickMenu} />;
+          return (
+            <NavItemWithIndicator
+              key={`${child.label}-${index}`}
+              child={child}
+              index={index}
+              hasNew={hasNew.notice}
+              clickMenu={clickMenu}
+            />
+          );
         default:
-          return <RegularNavItem child={child} index={index} clickMenu={clickMenu} />;
+          return (
+            <RegularNavItem
+              key={`${child.label}-${index}`}
+              child={child}
+              index={index}
+              clickMenu={clickMenu}
+            />
+          );
       }
     },
     [hasNew, clickMenu]
@@ -75,13 +108,21 @@ const NavMenu = memo(() => {
   const clickMenu = useCallback(async () => {
     toggleMobile();
     // 관련된 모든 쿼리 무효화
-    await Promise.all([queryClient.invalidateQueries({ queryKey: ["approvalNew"] }), queryClient.invalidateQueries({ queryKey: ["noticeNew"] })]);
+    await Promise.all([
+      queryClient.invalidateQueries({ queryKey: ["approvalNew"] }),
+      queryClient.invalidateQueries({ queryKey: ["noticeNew"] }),
+    ]);
   }, [toggleMobile, queryClient]);
 
   return (
     <Box mt="xs">
       {MENU_ITEMS.map((item, index) => (
-        <NavItem key={`${item.label}-${index}`} hasNew={hasNew} item={item} clickMenu={clickMenu} />
+        <NavItem
+          key={`${item.label}-${index}`}
+          hasNew={hasNew}
+          item={item}
+          clickMenu={clickMenu}
+        />
       ))}
     </Box>
   );
