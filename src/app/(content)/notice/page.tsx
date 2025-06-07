a501f9c6b511d3edd53d06d7daac837bf020d2ca
aksel26 <kevinxkim2023@gmail.com>
2025-06-08 00:20:07 +0900

    공지사항 fetch suspense

--- src/app/(content)/notice/page.tsx
+++ src/app/(content)/notice/page.tsx
@@ -6,78 +6,109 @@ import LoadingView from "@/components/Global/view/LoadingView";
 import useGetNotices from "@/hooks/useGetNotices";
 import { TNotice } from "@/lib/types/notice";
 import { formatYYYYMMDD } from "@/utils/dateFomat";
-import { Affix, Badge, Button, Group, List, ListItem, Paper, Stack, Text, TextInput } from "@mantine/core";
+import {
+  Affix,
+  Badge,
+  Button,
+  Group,
+  List,
+  ListItem,
+  Paper,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
 import { IconPlus, IconSearch } from "@tabler/icons-react";
 import { usePathname, useRouter } from "next/navigation";
-import React, { useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import styles from "../../../styles/list.module.css";
 import { useDisclosure } from "@mantine/hooks";
 import CreateNotice from "@/components/notice/create";
-const Notice = () => {
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { getNotices } from "@/app/api/get/getApi";
+import Loading from "./loading";
+import NoticeList from "@/components/notice/list";
+// import { ListWrapper } from "@/components/content/welfare/ListWrapper";
+const Notice = ({ state }: any) => {
+  console.log("state: ", state);
   const router = useRouter();
   const pathName = usePathname();
   const [opened, { open, close }] = useDisclosure(false);
-  const [params, setParams] = useState({
-    perPage: 20,
-    pageNo: 1,
-  });
+  // const [params, setParams] = useState({
+  //   perPage: 20,
+  //   pageNo: 1,
+  // });
 
-  const { notices, isLoading, isError } = useGetNotices({ params });
+  // const { notices, isLoading, isError } = useGetNotices({ params });
+  // const { data, isLoading, isError } = useSuspenseQuery({
+  //   queryKey: ["notices", params],
+  //   queryFn: () => getNotices(params),
+  // });
+  // console.log("data: ", data);
 
   const goDetail = async (id: number) => router.push(`${pathName}/${id}`);
   const searchRef = useRef<HTMLInputElement>(null);
-  const search = (e: any) => {
-    if (e.key === "Enter" || e.key === "NumpadEnter" || e.type === "click") {
-      if (searchRef.current) {
-        const value = searchRef.current.value || "";
-        setParams((prev: any) => ({ ...prev, searchWord: value }));
-      }
-    }
-  };
+  // const search = (e: any) => {
+  //   if (e.key === "Enter" || e.key === "NumpadEnter" || e.type === "click") {
+  //     if (searchRef.current) {
+  //       const value = searchRef.current.value || "";
+  //       setParams((prev: any) => ({ ...prev, searchWord: value }));
+  //     }
+  //   }
+  // };
 
-  const ListWrapper = () => {
-    return (
-      <List spacing={0} size="sm" center>
-        {notices?.map((record: TNotice, index: number, arr: any) => {
-          return (
-            <React.Fragment key={index}>
-              <Items key={record.noticeIdx} record={record} />
-            </React.Fragment>
-          );
-        })}
-      </List>
-    );
-  };
+  // const ListWrapper = () => {
+  //   return (
+  //     <List spacing={0} size="sm" center>
+  //       {data.data.data.result?.map(
+  //         (record: TNotice, index: number, arr: any) => {
+  //           return (
+  //             <React.Fragment key={index}>
+  //               <Items key={record.noticeIdx} record={record} />
+  //             </React.Fragment>
+  //           );
+  //         }
+  //       )}
+  //     </List>
+  //   );
+  // };
 
-  const Items = ({ record }: { record: TNotice }) => (
-    <ListItem w={"100%"} onClick={() => goDetail(record.noticeIdx)} key={record.noticeIdx} className={styles.element} px={"sm"} py={"md"}>
-      <Stack gap={4}>
-        <Group>
-          <Text fz={"xs"}>{record.title}</Text>
-          {record.isNew && (
-            <Badge size="xs" variant="light">
-              New
-            </Badge>
-          )}
-        </Group>
-        <Group>
-          <Text c={"dimmed"} fz={"xs"}>
-            {record.creatorName}
-          </Text>
-          <Text c={"dimmed"} fz={"xs"}>
-            {formatYYYYMMDD(record.createdAt)}
-          </Text>
-        </Group>
-      </Stack>
-    </ListItem>
-  );
+  // const Items = ({ record }: { record: TNotice }) => (
+  //   <ListItem
+  //     w={"100%"}
+  //     onClick={() => goDetail(record.noticeIdx)}
+  //     key={record.noticeIdx}
+  //     className={styles.element}
+  //     px={"sm"}
+  //     py={"md"}
+  //   >
+  //     <Stack gap={4}>
+  //       <Group>
+  //         <Text fz={"xs"}>{record.title}</Text>
+  //         {record.isNew && (
+  //           <Badge size="xs" variant="light">
+  //             New
+  //           </Badge>
+  //         )}
+  //       </Group>
+  //       <Group>
+  //         <Text c={"dimmed"} fz={"xs"}>
+  //           {record.creatorName}
+  //         </Text>
+  //         <Text c={"dimmed"} fz={"xs"}>
+  //           {formatYYYYMMDD(record.createdAt)}
+  //         </Text>
+  //       </Group>
+  //     </Stack>
+  //   </ListItem>
+  // );
 
-  const renderContent = () => {
-    if (isLoading) return <LoadingView />;
-    if (isError) return <ErrorView>공지/일정 내역을 불러오는 중 문제가 발생했습니다.</ErrorView>;
-    if (notices?.length === 0) return <EmptyView />;
-    return <ListWrapper />;
-  };
+  // const renderContent = () => {
+  //   if (isLoading) return <LoadingView />;
+  //   if (isError) return <ErrorView>공지/일정 내역을 불러오는 중 문제가 발생했습니다.</ErrorView>;
+  //   if (notices?.length === 0) return <EmptyView />;
+  //   return <ListWrapper />;
+  // };
 
   return (
     <PageContainer>
@@ -96,16 +127,34 @@ const Notice = () => {
         <Stack gap={"xs"}>
           <Group justify="space-between" align="center">
             <Group wrap="nowrap">
-              <TextInput onKeyUp={(e) => search(e)} ref={searchRef} placeholder="제목 또는 내용을 입력하세요." miw={240} leftSection={<IconSearch size={18} />} />
-              <Button onClick={search} onKeyUp={(e) => search(e)} variant="light">
+              <TextInput
+                // onKeyUp={(e) => search(e)}
+                ref={searchRef}
+                placeholder="제목 또는 내용을 입력하세요."
+                miw={240}
+                leftSection={<IconSearch size={18} />}
+              />
+              <Button
+                // onClick={search}
+                // onKeyUp={(e) => search(e)}
+                variant="light"
+              >
                 검색
               </Button>
             </Group>
-            <Button visibleFrom="md" leftSection={<IconPlus size={15} />} onClick={open}>
+            <Button
+              visibleFrom="md"
+              leftSection={<IconPlus size={15} />}
+              onClick={open}
+            >
               등록
             </Button>
           </Group>
-          {renderContent()}
+
+          <Suspense fallback={<Loading />}>
+            {/* {renderContent()} */}
+            <NoticeList />
+          </Suspense>
         </Stack>
       </Paper>
       <Affix position={{ bottom: 20, right: 20 }} hiddenFrom="md">
---
+++ src/components/notice/list/index.tsx
@@ -0,0 +1,68 @@
+import { getNotices } from "@/app/api/get/getApi";
+import { TNotice } from "@/lib/types/notice";
+import { formatYYYYMMDD } from "@/utils/dateFomat";
+import { Badge, Group, List, ListItem, Stack, Text } from "@mantine/core";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
+
+const NoticeList = () => {
+  const ListWrapper = () => {
+    return (
+      <List spacing={0} size="sm" center>
+        {data.data.data.result?.map(
+          (record: TNotice, index: number, arr: any) => {
+            return (
+              <React.Fragment key={index}>
+                <Items key={record.noticeIdx} record={record} />
+              </React.Fragment>
+            );
+          }
+        )}
+      </List>
+    );
+  };
+
+  const Items = ({ record }: { record: TNotice }) => (
+    <ListItem
+      w={"100%"}
+      //   onClick={() => goDetail(record.noticeIdx)}
+      key={record.noticeIdx}
+      //   className={styles.element}
+      px={"sm"}
+      py={"md"}
+    >
+      <Stack gap={4}>
+        <Group>
+          <Text fz={"xs"}>{record.title}</Text>
+          {record.isNew && (
+            <Badge size="xs" variant="light">
+              New
+            </Badge>
+          )}
+        </Group>
+        <Group>
+          <Text c={"dimmed"} fz={"xs"}>
+            {record.creatorName}
+          </Text>
+          <Text c={"dimmed"} fz={"xs"}>
+            {formatYYYYMMDD(record.createdAt)}
+          </Text>
+        </Group>
+      </Stack>
+    </ListItem>
+  );
+
+  const [params, setParams] = useState({
+    perPage: 20,
+    pageNo: 1,
+  });
+
+  // const { notices, isLoading, isError } = useGetNotices({ params });
+  const { data, isLoading, isError } = useSuspenseQuery({
+    queryKey: ["notices", params],
+    queryFn: () => getNotices(params),
+  });
+  return <ListWrapper />;
+};
+
+export default NoticeList;
