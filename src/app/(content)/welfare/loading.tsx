737fc5ef92ea6a2e52535c7eeab3074bb078eb1f
aksel26 <kevinxkim2023@gmail.com>
2025-06-11 16:48:49 +0900

    복지포인트 prefetch, suspense

--- src/app/(content)/welfare/loading.tsx
+++ src/app/(content)/welfare/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from "@mantine/core";
 // app/dashboard/loading.js
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-32">
       <Loader />
     </div>
   );
--- src/app/(content)/welfare/welfarePoint/page.tsx
+++ src/app/(content)/welfare/welfarePoint/page.tsx
@@ -1,39 +1,20 @@
 "use client";
 
-import { TopTitleWelfare } from "@/components/content/welfare/TopTitleWelfare";
-import { UsedListWelfare } from "@/components/content/welfare/UsedListWelfare";
+import { WelfareFetchWrapper } from "@/components/content/welfare/WelfareFetchWrapper";
 import WelfareInputForm from "@/components/content/welfare/WelfareInputForm";
-import DurationSelect from "@/components/Global/dateSelect/DurationSelect";
-import YearSelect from "@/components/Global/dateSelect/YearSelect";
-import { groupByDate } from "@/utils/welfare/groupByDate";
-import { Container, Grid, GridCol, Group, Paper, Title } from "@mantine/core";
-import { useQuery } from "@tanstack/react-query";
-import dayjs from "dayjs";
-import { useState } from "react";
-import * as api from "../../../api/get/getApi";
+import PageContainer from "@/components/Global/container";
+import { Grid, GridCol, Paper, Title } from "@mantine/core";
+import { Suspense } from "react";
+import Loading from "../loading";
+
 const WelfareMain = () => {
-  const [params, setParams] = useState({
-    year: dayjs().year().toString(),
-    halfYear: Number(dayjs().month() + 1) > 6 ? "H2" : "H1",
-  });
-
-  const { data, isLoading, isError } = useQuery({ queryKey: ["welfares", params], queryFn: () => api.getWelfares(params) });
-
-  const welfares = data && groupByDate(data?.data.data.welfares);
-  const welfareStats = data?.data.data.welfareStats;
-
   return (
-    <Container fluid p={"lg"} style={{ scrollPaddingBottom: "52px", overflowY: "auto", scrollSnapType: "y mandatory" }}>
+    <PageContainer>
       <Grid>
         <GridCol span={{ base: 12, md: 8 }}>
-          <TopTitleWelfare stats={welfareStats} isLoading={isLoading} isError={isError} />
-
-          <Group mt={"lg"}>
-            <YearSelect setParams={setParams} w={120} />
-            <DurationSelect setParams={setParams} w={120} />
-          </Group>
-
-          <UsedListWelfare welfares={welfares} isLoading={isLoading} isError={isError} />
+          <Suspense fallback={<Loading />}>
+            <WelfareFetchWrapper />
+          </Suspense>
         </GridCol>
         <GridCol span={{ base: 12, md: 4 }} visibleFrom="md">
           <Paper bg={"white"} py="lg" px={"lg"} radius={"lg"}>
@@ -45,7 +26,7 @@ const WelfareMain = () => {
           </Paper>
         </GridCol>
       </Grid>
-    </Container>
+    </PageContainer>
   );
 };
 
