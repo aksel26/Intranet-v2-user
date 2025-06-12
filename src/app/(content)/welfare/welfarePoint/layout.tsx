5ad0e5bafe52c069d95e6b8bb15ae6090f827df5
aksel26 <kevinxkim2023@gmail.com>
2025-06-12 10:52:27 +0900

    복포 fetch 컴포넌트 제외 서버컴포넌트 이동

--- src/app/(content)/welfare/welfarePoint/layout.tsx
+++ src/app/(content)/welfare/welfarePoint/layout.tsx
@@ -1,18 +1,12 @@
+import WelfareInputForm from "@/components/content/welfare/WelfareInputForm";
 import { createServerApiClient } from "@/lib/axios/server-api";
 import { getQueryClient } from "@/lib/query-client/get-query-client";
-import {
-  dehydrate,
-  HydrationBoundary,
-  queryOptions,
-} from "@tanstack/react-query";
+import { Grid, GridCol, Paper, Title } from "@mantine/core";
+import { dehydrate, HydrationBoundary, queryOptions } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import React from "react";
 
-export default async function layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function layout({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
   const apiClient = await createServerApiClient();
 
@@ -35,7 +29,18 @@ export default async function layout({
   await queryClient.prefetchQuery(prefetchOption);
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      {children}
+      <Grid>
+        <GridCol span={{ base: 12, md: 8 }}>{children}</GridCol>
+        <GridCol span={{ base: 12, md: 4 }} visibleFrom="md">
+          <Paper bg={"white"} py="lg" px={"lg"} radius={"lg"}>
+            <Title order={5} mb={"md"}>
+              복지포인트 입력
+            </Title>
+
+            <WelfareInputForm />
+          </Paper>
+        </GridCol>
+      </Grid>
     </HydrationBoundary>
   );
 }
--- src/app/(content)/welfare/welfarePoint/page.tsx
+++ src/app/(content)/welfare/welfarePoint/page.tsx
@@ -1,32 +1,14 @@
 "use client";
 
 import { WelfareFetchWrapper } from "@/components/content/welfare/WelfareFetchWrapper";
-import WelfareInputForm from "@/components/content/welfare/WelfareInputForm";
-import PageContainer from "@/components/Global/container";
-import { Grid, GridCol, Paper, Title } from "@mantine/core";
 import { Suspense } from "react";
 import Loading from "../loading";
 
 const WelfareMain = () => {
   return (
-    <PageContainer>
-      <Grid>
-        <GridCol span={{ base: 12, md: 8 }}>
-          <Suspense fallback={<Loading />}>
-            <WelfareFetchWrapper />
-          </Suspense>
-        </GridCol>
-        <GridCol span={{ base: 12, md: 4 }} visibleFrom="md">
-          <Paper bg={"white"} py="lg" px={"lg"} radius={"lg"}>
-            <Title order={5} mb={"md"}>
-              복지포인트 입력
-            </Title>
-
-            <WelfareInputForm />
-          </Paper>
-        </GridCol>
-      </Grid>
-    </PageContainer>
+    <Suspense fallback={<Loading />}>
+      <WelfareFetchWrapper />
+    </Suspense>
   );
 };
 
