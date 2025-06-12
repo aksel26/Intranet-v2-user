0349337063829df706377964ea30e1065ee59e7c
aksel26 <kevinxkim2023@gmail.com>
2025-06-12 10:52:22 +0900

    식대 fetch 컴포넌트 제외 서버컴포넌트 이동

--- src/app/(content)/welfare/meal/layout.tsx
+++ src/app/(content)/welfare/meal/layout.tsx
@@ -1,18 +1,12 @@
+import Calendar from "@/components/content/meal/Calendar";
 import { createServerApiClient } from "@/lib/axios/server-api";
 import { getQueryClient } from "@/lib/query-client/get-query-client";
-import {
-  dehydrate,
-  HydrationBoundary,
-  queryOptions,
-} from "@tanstack/react-query";
+import { Grid, GridCol } from "@mantine/core";
+import { dehydrate, HydrationBoundary, queryOptions } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import React from "react";
 
-export default async function layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function layout({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
   const apiClient = await createServerApiClient();
 
@@ -34,7 +28,12 @@ export default async function layout({
   await queryClient.prefetchQuery(prefetchOption);
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      {children}
+      <Grid>
+        <GridCol span={{ base: 12, md: 8 }}>{children}</GridCol>
+        <GridCol span={{ base: 12, md: 4 }}>
+          <Calendar />
+        </GridCol>
+      </Grid>
     </HydrationBoundary>
   );
 }
--- src/app/(content)/welfare/meal/page.tsx
+++ src/app/(content)/welfare/meal/page.tsx
@@ -1,26 +1,14 @@
 "use client";
 
-import Calendar from "@/components/content/meal/Calendar";
 import { MealFetchWrapper } from "@/components/content/meal/MealFetchWrapper";
-import PageContainer from "@/components/Global/container";
-import { Grid, GridCol } from "@mantine/core";
 import { Suspense } from "react";
 import Loading from "../loading";
 
 const Main = () => {
   return (
-    <PageContainer>
-      <Grid>
-        <GridCol span={{ base: 12, md: 8 }}>
-          <Suspense fallback={<Loading />}>
-            <MealFetchWrapper />
-          </Suspense>
-        </GridCol>
-        <GridCol span={{ base: 12, md: 4 }}>
-          <Calendar />
-        </GridCol>
-      </Grid>
-    </PageContainer>
+    <Suspense fallback={<Loading />}>
+      <MealFetchWrapper />
+    </Suspense>
   );
 };
 
