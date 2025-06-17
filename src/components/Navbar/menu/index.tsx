1996966bb19453419b7c07f5c5abbbb3b4f67aed
aksel26 <kevinxkim2023@gmail.com>
2025-06-17 18:02:35 +0900

    height관련 toggle 옵션 수정

--- src/components/Navbar/menu/index.tsx
+++ src/components/Navbar/menu/index.tsx
@@ -14,42 +14,32 @@ interface NavItemWithIndicatorProps {
 }
 
 // Indicator가 있는 NavLink 컴포넌트를 별도로 분리
-const NavItemWithIndicator = memo(
-  ({ child, index, hasNew, clickMenu }: NavItemWithIndicatorProps) => (
-    <NavLink
-      pos="relative"
-      onClick={clickMenu}
-      key={`${child.label}-${index}`}
-      component={Link}
-      href={child.href}
-      label={
-        <Group>
-          <Text fz="sm">{child.label}</Text>
-          {hasNew && (
-            <Badge size="xs" variant="light">
-              New
-            </Badge>
-          )}
-        </Group>
-      }
-    />
-  )
-);
+const NavItemWithIndicator = memo(({ child, index, hasNew, clickMenu }: NavItemWithIndicatorProps) => (
+  <NavLink
+    pos="relative"
+    onClick={clickMenu}
+    key={`${child.label}-${index}`}
+    component={Link}
+    href={child.href}
+    label={
+      <Group>
+        <Text fz="sm">{child.label}</Text>
+        {hasNew && (
+          <Badge size="xs" variant="light">
+            New
+          </Badge>
+        )}
+      </Group>
+    }
+  />
+));
 
 NavItemWithIndicator.displayName = "NavItemWithIndicator";
 
 // 일반 NavLink 컴포넌트
-const RegularNavItem = memo(
-  ({ child, index, clickMenu }: Omit<NavItemWithIndicatorProps, "hasNew">) => (
-    <NavLink
-      onClick={clickMenu}
-      key={`${child.label}-${index}`}
-      component={Link}
-      href={child.href}
-      label={child.label}
-    />
-  )
-);
+const RegularNavItem = memo(({ child, index, clickMenu }: Omit<NavItemWithIndicatorProps, "hasNew">) => (
+  <NavLink onClick={clickMenu} key={`${child.label}-${index}`} component={Link} href={child.href} label={child.label} />
+));
 
 RegularNavItem.displayName = "RegularNavItem";
 
@@ -58,34 +48,11 @@ const NavItem = memo(({ item, clickMenu, hasNew }: NavItemProps) => {
     (child: any, index: number) => {
       switch (child.label) {
         case "결재/승인":
-          return (
-            <NavItemWithIndicator
-              key={`${child.label}-${index}`}
-              child={child}
-              index={index}
-              hasNew={hasNew.approval}
-              clickMenu={clickMenu}
-            />
-          );
+          return <NavItemWithIndicator key={`${child.label}-${index}`} child={child} index={index} hasNew={hasNew.approval} clickMenu={clickMenu} />;
         case "공지/일정":
-          return (
-            <NavItemWithIndicator
-              key={`${child.label}-${index}`}
-              child={child}
-              index={index}
-              hasNew={hasNew.notice}
-              clickMenu={clickMenu}
-            />
-          );
+          return <NavItemWithIndicator key={`${child.label}-${index}`} child={child} index={index} hasNew={hasNew.notice} clickMenu={clickMenu} />;
         default:
-          return (
-            <RegularNavItem
-              key={`${child.label}-${index}`}
-              child={child}
-              index={index}
-              clickMenu={clickMenu}
-            />
-          );
+          return <RegularNavItem key={`${child.label}-${index}`} child={child} index={index} clickMenu={clickMenu} />;
       }
     },
     [hasNew, clickMenu]
@@ -103,26 +70,18 @@ NavItem.displayName = "NavItem";
 const NavMenu = memo(() => {
   const queryClient = useQueryClient();
   const hasNew = useHasNew();
-  const toggleMobile = useNavStore((state) => state.toggleMobile);
+  const setMobileClose = useNavStore((state) => state.setMobileClose);
 
   const clickMenu = useCallback(async () => {
-    toggleMobile();
+    setMobileClose();
     // 관련된 모든 쿼리 무효화
-    await Promise.all([
-      queryClient.invalidateQueries({ queryKey: ["approvalNew"] }),
-      queryClient.invalidateQueries({ queryKey: ["noticeNew"] }),
-    ]);
-  }, [toggleMobile, queryClient]);
+    await Promise.all([queryClient.invalidateQueries({ queryKey: ["approvalNew"] }), queryClient.invalidateQueries({ queryKey: ["noticeNew"] })]);
+  }, [setMobileClose, queryClient]);
 
   return (
     <Box mt="xs">
       {MENU_ITEMS.map((item, index) => (
-        <NavItem
-          key={`${item.label}-${index}`}
-          hasNew={hasNew}
-          item={item}
-          clickMenu={clickMenu}
-        />
+        <NavItem key={`${item.label}-${index}`} hasNew={hasNew} item={item} clickMenu={clickMenu} />
       ))}
     </Box>
   );
