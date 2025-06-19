d625fabf7738a91eb076180a73c79f56ce7cfec3
aksel26 <kevinxkim2023@gmail.com>
2025-06-19 18:35:52 +0900

    suspenseQuery

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { LoadingOverlay } from "@mantine/core";
-import React from "react";
-
-export default function loading() {
-  return <LoadingOverlay visible zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />;
-}
--- src/components/Navbar/userInfoCard/index.tsx
+++ src/components/Navbar/userInfoCard/index.tsx
@@ -4,7 +4,7 @@ import LoadingView from "@/components/Global/view/LoadingView";
 import { myInfoStore } from "@/lib/store/myInfoStore";
 import { getDDayCount } from "@/utils/date/dDayCount";
 import { Box, Card, Flex, Group, Skeleton, Stack, Text } from "@mantine/core";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import { useSession } from "next-auth/react";
 import React, { useCallback, useEffect, useState } from "react";
@@ -16,7 +16,7 @@ const UserInfoCard = () => {
 
   const [queryKey, setQueryKey] = useState<string>();
 
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading } = useSuspenseQuery({
     queryKey: ["me", { userIdx: queryKey }],
     queryFn: () => getMe(),
   });
@@ -28,50 +28,53 @@ const UserInfoCard = () => {
     if (session.data) setQueryKey(session?.data.user.id);
   }, [data, session]);
 
-  const renderContent = () => {
-    if (isError) return <ErrorViewSmall>유저 정보를 불러오는 중 문제가 발생했습니다.</ErrorViewSmall>;
+  const { userName, gradeName, hqName, teamName, userEmail, joinDate } = myInfo;
 
-    if (isLoading) return <LoadingView />;
-    const { userName, gradeName, hqName, teamName, userEmail, joinDate } = myInfo;
+  // const renderContent = () => {
+  //   if (isError) return <ErrorViewSmall>유저 정보를 불러오는 중 문제가 발생했습니다.</ErrorViewSmall>;
 
-    return (
-      <Skeleton visible={isLoading}>
-        <Card padding="md" radius="md" bg={"primary.0"}>
-          <Flex direction={"column"} w={"100%"} columnGap={"xl"} style={{ position: "relative", borderRadius: 7 }}>
-            <Flex direction={"column"} rowGap={"md"} w={"100%"}>
-              <Box>
-                <Text fz={"lg"} fw={500} c={"primary.9"}>
-                  {userName}
-                  <Text fz={"sm"} c={"primary.9"} component="span" ml={5}>
-                    {gradeName}
-                  </Text>
+  //
+
+  //   return (
+
+  //   );
+  // };
+
+  return (
+    <Skeleton visible={isLoading}>
+      <Card padding="md" radius="md" bg={"primary.0"}>
+        <Flex direction={"column"} w={"100%"} columnGap={"xl"} style={{ position: "relative", borderRadius: 7 }}>
+          <Flex direction={"column"} rowGap={"md"} w={"100%"}>
+            <Box>
+              <Text fz={"lg"} fw={500} c={"primary.9"}>
+                {userName}
+                <Text fz={"sm"} c={"primary.9"} component="span" ml={5}>
+                  {gradeName}
                 </Text>
-                <Group gap={"xs"} mt={3}>
-                  <Text fz={"sm"} c={"primary.9"} component="span">
-                    {hqName}
-                  </Text>
-                  <Text fz={"sm"} c={"primary.9"} component="span">
-                    {teamName || ""}
-                  </Text>
-                </Group>
-              </Box>
-
-              <Group justify="space-between" align="end">
-                <Text c={"primary.9"} size={"xs"}>
-                  {userEmail}
+              </Text>
+              <Group gap={"xs"} mt={3}>
+                <Text fz={"sm"} c={"primary.9"} component="span">
+                  {hqName}
                 </Text>
-                <Text c={"primary.9"} size={"xs"}>
-                  {getDDayCount(joinDate)}
+                <Text fz={"sm"} c={"primary.9"} component="span">
+                  {teamName || ""}
                 </Text>
               </Group>
-            </Flex>
-          </Flex>
-        </Card>
-      </Skeleton>
-    );
-  };
+            </Box>
 
-  return <>{renderContent()}</>;
+            <Group justify="space-between" align="end">
+              <Text c={"primary.9"} size={"xs"}>
+                {userEmail}
+              </Text>
+              <Text c={"primary.9"} size={"xs"}>
+                {getDDayCount(joinDate)}
+              </Text>
+            </Group>
+          </Flex>
+        </Flex>
+      </Card>
+    </Skeleton>
+  );
 };
 
 export default UserInfoCard;
