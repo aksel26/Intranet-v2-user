798b37a25a6006fab8f93c9de86de88f9774e622
aksel26 <kevinxkim2023@gmail.com>
2025-06-19 18:26:22 +0900

    내 정보 수정 페이지 ssr

--- src/app/(content)/myInfo/layout.tsx
+++ src/app/(content)/myInfo/layout.tsx
@@ -1,6 +1,7 @@
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import React from "react";
 import * as api from "@/app/api/get/getApi";
+import { Stack, Text, Title } from "@mantine/core";
 
 export default async function layout({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
@@ -8,5 +9,15 @@ export default async function layout({ children }: { children: React.ReactNode }
     queryKey: ["me"],
     queryFn: () => api.getMe(),
   });
-  return <HydrationBoundary state={dehydrate(queryClient)}>{children}</HydrationBoundary>;
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Stack gap={1} mb="xs">
+        <Title order={4}>내 정보 수정</Title>
+        <Text c={"gray.6"} fz={"sm"}>
+          비밀번호 변경, 기본 정보 수정 등을 할 수 있습니다.
+        </Text>
+      </Stack>
+      {children}
+    </HydrationBoundary>
+  );
 }
--- src/app/(content)/myInfo/page.tsx
+++ src/app/(content)/myInfo/page.tsx
@@ -1,18 +1,13 @@
 "use client";
-import PageContainer from "@/components/Global/container";
 import UpdateBaseInfo from "@/components/myInfo/baseInfo";
 import UpdatePassword from "@/components/myInfo/password";
-import { Text, Title } from "@mantine/core";
 
 const MyInfo = () => {
   return (
-    <PageContainer>
-      <Title order={4}>내 정보 수정</Title>
-
+    <>
       <UpdateBaseInfo />
-
       <UpdatePassword />
-    </PageContainer>
+    </>
   );
 };
 
