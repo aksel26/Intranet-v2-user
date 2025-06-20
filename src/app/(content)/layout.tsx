2c1a7dcabb65645a0eee9407b3ce2297e2f5a633
aksel26 <kevinxkim2023@gmail.com>
2025-06-20 11:36:12 +0900

    header 코드 분리, P&C 만 admin리다이렉트 버튼 노출

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -1,21 +1,16 @@
 "use client";
-import { Anchor, AppShell, Box, Burger, Button, Group, Image, Popover, ScrollArea, Stack, Text } from "@mantine/core";
+import { AppShell, Box, Group, ScrollArea, Text } from "@mantine/core";
 // import Image from "next/image";
-import NextImage from "next/image";
-import React, { memo, useEffect } from "react";
-import myImage from "/public/images/ACG_LOGO_GRAY.png";
+import React, { memo } from "react";
 
+import Header from "@/components/GNB/Header";
 import AttendanceInfo from "@/components/Navbar/attendanceInfo";
 import LogoutButton from "@/components/Navbar/logout";
 import NavMenu from "@/components/Navbar/menu";
 import UserInfoCard from "@/components/Navbar/userInfoCard";
-import { BOOKMARKS } from "@/lib/bookmark";
 import { useNavStore } from "@/lib/store/toggleStore";
-import { IconBookmarkFilled, IconExternalLink } from "@tabler/icons-react";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
-import Link from "next/link";
-import { ADMIN_URL } from "@/lib/enums";
 
 dayjs.locale("ko");
 
@@ -24,21 +19,6 @@ const MemoizedNavMenu = memo(NavMenu);
 
 export default function ContentLayout({ children }: { children: React.ReactNode }) {
   const mobileOpened = useNavStore((state) => state.mobileOpened);
-  const toggleMobile = useNavStore((state) => state.toggleMobile);
-  const setMobileClose = useNavStore((state) => state.setMobileClose);
-
-  useEffect(() => {
-    if (mobileOpened) {
-      document.body.classList.add("navbar-open");
-    } else {
-      document.body.classList.remove("navbar-open");
-    }
-
-    // cleanup
-    return () => {
-      document.body.classList.remove("navbar-open");
-    };
-  }, [mobileOpened]);
 
   return (
     <AppShell
@@ -52,37 +32,7 @@ export default function ContentLayout({ children }: { children: React.ReactNode 
       padding="md"
     >
       <AppShell.Header withBorder={false}>
-        <Group justify="space-between" align="center" h={"100%"} px="md" wrap="nowrap">
-          <Group>
-            <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-            <Link href={"/main"} onClick={() => setMobileClose()} style={{ textDecoration: "none" }}>
-              <Image component={NextImage} src={myImage} alt="My image" fit="contain" h={20} w={80} style={{ cursor: "pointer" }} />
-            </Link>
-          </Group>
-          <Group>
-            <Button gradient={{ from: "teal", to: "blue", deg: 233 }} size="xs" leftSection={<IconExternalLink size={16} />} variant="gradient" onClick={() => window.open(ADMIN_URL, "_blank")}>
-              Admin
-            </Button>
-            <Popover width={200} position="bottom" withArrow shadow="md">
-              <Popover.Target>
-                <Button visibleFrom="md" size="sm" variant="subtle" leftSection={<IconBookmarkFilled size={15} color="#f7c401" />}>
-                  북마크
-                </Button>
-              </Popover.Target>
-              <Popover.Dropdown>
-                <ScrollArea h={200}>
-                  <Stack gap={"sm"}>
-                    {BOOKMARKS.map((bookmark) => (
-                      <Anchor underline="hover" size="xs" href={bookmark.value} target="_blank" key={bookmark.value}>
-                        {bookmark.label}
-                      </Anchor>
-                    ))}
-                  </Stack>
-                </ScrollArea>
-              </Popover.Dropdown>
-            </Popover>
-          </Group>
-        </Group>
+        <Header />
       </AppShell.Header>
 
       <AppShell.Navbar p="md" withBorder={false}>
